f9ae7855e71c49a521e795781dc66af29b466a5f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:57:52 -0300

    Saved natalia/main.py at Timestamp: 2019-08-27 13:57:51

--- natalia/main.py
+++ natalia/main.py
@@ -1,5 +1,6 @@
 # margaret.natalia.main.py
 from _spy.vitollino.main import Cena
+from _parisa.main import sereiamonstro
 CENA = "http://i.imgur.com/loO50ff.jpg"
 ARTEFATO = "https://i.imgur.com/rGbY5XC.jpg"
 templo = Cena(CENA)
@@ -8,3 +9,5 @@ templo.direita = artefato
 artefato.esquerda = templo
 templo.vai()
 CENA = "http://i.imgur.com/loO50ff.jpg"
+PEDRAS = "https://i.imgur.com/4ftVqRs.jpg"
+templo = Cena(CENA)

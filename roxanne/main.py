1d8c21d86ece0e08f6ff18d62e809a8405302b6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:53:33 -0300

    Created roxanne/main.py at Timestamp: 2019-08-27 13:53:33

---
+++ roxanne/main.py
@@ -0,0 +1,10 @@
+# margaret.roxanne.main.py
+from _spy.vitollino.main import Cena 
+CENA = "https://i.imgur.com/XY8get8.jpg"
+MONSTRO = "https://i.imgur.com/ZOoZ2aQ.jpg"
+caverna = Cena(CENA)
+monstro = Cena(MONSTRO)
+caverna.direita = tesouro
+tesouro.esquerda = caverna
+caverna.vai()
+

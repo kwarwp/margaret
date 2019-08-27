f234dd0ec914736cb83a4601c2aa67c5567eb26f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:52:13 -0300

    Saved kellee/main.py at Timestamp: 2019-08-27 13:52:12

--- kellee/main.py
+++ kellee/main.py
@@ -3,5 +3,8 @@ from _spy.vitollino.main import Cena
 CENA = "https://i.imgur.com/F0pWLRM.png"
 Cena(CENA).vai()
 TESOURO = "https://i.imgur.com/Z2CD83L.jpg"
-templo = Cena(CENA).vai()
-tesouro = Cena(TESOURO).vai()
+templo = Cena(CENA)
+tesouro = Cena(TESOURO)
+templo.direita = tesouro
+tesouro.esquerda = templo
+templo.vai()

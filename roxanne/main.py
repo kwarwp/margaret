506f61859c6a9d79ec3df8bd5e009167b46a8fa8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 12:59:33 -0300

    Saved roxanne/main.py at Timestamp: 2019-08-27 15:59:32

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,13 +2,13 @@
 from _spy.vitollino.main import Cena 
 CENA = "https://i.imgur.com/XY8get8.jpg"
 MONSTRO = "https://i.imgur.com/ZOoZ2aQ.jpg"
-TESOURO = "https://i.imgur.com/4L5y1To.jpg"
+OURO = "https://i.imgur.com/4L5y1To.jpg"
 caverna = Cena(CENA)
 monstro = Cena(MONSTRO)
-tesouro = Cena(TESOURO)
+ouro = Cena(OURO)
 caverna.direita = monstro
 monstro.esquerda = caverna
 caverna.vai()
-monstro.direira = tesouro
-tesouro.esquerda = monstro
+monstro.direira = ouro
+ouro.esquerda = monstro
 monstro.vai()

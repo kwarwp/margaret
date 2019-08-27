622fb7c9aee3ef8ea04404afacc3676be3167bce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:11:06 -0300

    Saved natalia/main.py at Timestamp: 2019-08-27 14:11:05

--- natalia/main.py
+++ natalia/main.py
@@ -1,6 +1,6 @@
 # margaret.natalia.main.py
 from _spy.vitollino.main import Cena
-from parisa.main import sereiamonstro
+from parisa.main import SEREIAMONSTRO
 CENA = "http://i.imgur.com/loO50ff.jpg"
 ARTEFATO = "https://i.imgur.com/rGbY5XC.jpg"
 PEDRAS = "https://i.imgur.com/4ftVqRs.jpg"

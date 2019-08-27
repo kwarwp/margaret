49ef0e9969f49eed62fc6640b79e5c4239b53c1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:48:15 -0300

    Created kellee/main.py at Timestamp: 2019-08-27 13:48:15

---
+++ kellee/main.py
@@ -0,0 +1,7 @@
+# margaret.kellee.main.py
+from _spy.vitollino.main import Cena
+CENA = "https://i.imgur.com/F0pWLRM.png"
+Cena(CENA).vai()
+TESOURO = "https://i.imgur.com/Z2CD83L.jpg"
+templo = Cena(CENA).vai()
+tesouro = Cena(TESOURO).vai()

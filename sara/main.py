ecb39f80775cdc868ad23db79576c6be023f8b39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:12:18 -0300

    Saved sara/main.py at Timestamp: 2019-08-27 14:12:18

--- sara/main.py
+++ sara/main.py
@@ -1,17 +1,20 @@
 # margaret.sara.main.py
 from _spy.vitollino.main import Cena
 CENA = "https://i.imgur.com/ZzWag8V.jpg"
-TESOURO = "https://i.imgur.com/0t0j5Ne.jpg"
-templo = Cena(CENA).vai()
-tesouro = Cena(TESOURO).vai()
-templo.direita = tesouro
-tesouro.esquerda = templo
+ARTEFATO = "https://i.imgur.com/0t0j5Ne.jpg"
 MONSTRO = "https://i.imgur.com/zwbxMeu.jpg"
-monstro = Cena(MONSTRO).vai()
-tesouro.direita = monstro
-monstro.esquerda = tesouro
 PEDRAS = "https://i.imgur.com/fvnFDJl.jpg"
-pedras = Cena(PEDRAS).vai()
-monstro.direita = pedras
-pedras.esquerda = monstro
-templo.vai()
+
+class Inca:
+    def inicia(self):
+        templo = Cena(CENA).vai()
+        artefato = Cena(ARTEFATO).vai()
+        templo.direita = artefato
+        artefato.esquerda = templo
+        monstro = Cena(MONSTRO).vai()
+        artefato.direita = monstro
+        monstro.esquerda = artefato
+        pedras = Cena(PEDRAS).vai()
+        monstro.direita = pedras
+        pedras.esquerda = monstro
+        templo.vai()

e9550289803ffdc326a0221888eb4a4f28326213
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:57:24 -0300

    Created libby/__log__.py at Timestamp: 2019-08-27 13:57:24

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 27 2019 10:57:30.492 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module stacy.main line 5
+  ARTEFATO = 
+              ^
+SyntaxError: invalid syntax
+'''},

d61fd8c11375cd4f945b3ba9df30b92ffc4b8eec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:06:35 -0300

    Saved angie/main.py at Timestamp: 2019-08-27 14:06:34

--- angie/main.py
+++ angie/main.py
@@ -2,9 +2,15 @@
 from _spy.vitollino.main import Cena
 CENA = "https://i.imgur.com/6NNu6ps.jpg"
 TESOURO = "https://i.imgur.com/nBbQxbD.jpg"
+COBRA = "https://i.imgur.com/ow7s2gK.jpg"
 templo = Cena (CENA).vai()
 tesouro = Cena(TESOURO).vai()
+cobra = Cena (COBRA).vai()
 templo.direita = tesouro
 templo.vai()
 tesouro.esquerda = templo
+templo.vai()
+tesouro.direita = cobra
+templo.vai()
+cobra.esquerda = tesouro
 templo.vai()

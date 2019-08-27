c446d6be6179decbd0fe9a7aecc95adc84c92011
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:08:35 -0300

    Saved kathryn/main.py at Timestamp: 2019-08-27 14:08:34

--- kathryn/main.py
+++ kathryn/main.py
@@ -4,7 +4,11 @@ CENA = "http://lorempixel/300/300/city/1"
 TESOURO = "https://i.imgur.com/DsncQRP.jpg"
 MUMIA = "https://i.imgur.com/ardMlqr.jpg"
 templo = Cena(CENA)
-tesouro = Cena(TESOURO)
+tesouro = Cena(TESOURO) 
+mumia = Cena(MUMIA)
 templo.direita = tesouro 
 tesouro.esquerda = templo 
 templo.vai()
+tesouro.direita = mumia 
+mumia.esquerda = tesouro 
+mumia.vai()

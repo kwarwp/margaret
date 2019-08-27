99e48b613e96b508922a2513cdbd4b0700565012
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:11:30 -0300

    Saved lisa/main.py at Timestamp: 2019-08-27 14:11:29

--- lisa/main.py
+++ lisa/main.py
@@ -3,10 +3,17 @@ from _spy.vitollino.main import Cena
 from stacy.main import fogo
 CENA = "https://i.imgur.com/7GZetDn.jpg"
 TESOURO = "https://i.imgur.com/h8MfuRD.jpg"
-templo = Cena(CENA)
-tesouro = Cena(TESOURO)
-templo.direita = tesouro
-tesouro.esquerda = templo
-tesouro.direita = fogo
-fogo.esquerda = tesouro
-templo.vai()
+
+class Inca:
+    def inicia(self):
+        templo = Cena(CENA)
+        tesouro = Cena(TESOURO)
+        templo.direita = tesouro
+        tesouro.esquerda = templo
+        tesouro.direita = fogo
+        fogo.esquerda = tesouro
+        templo.vai()
+
+inca = Inca()
+inca.inicia()
+    

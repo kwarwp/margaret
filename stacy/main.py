f304ccb4e9e265ea2908faa50b7dba193cbb4bd5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 20:51:43 -0300

    Saved stacy/main.py at Timestamp: 2019-08-27 23:51:43

--- stacy/main.py
+++ stacy/main.py
@@ -1,14 +1,42 @@
 # margaret.stacy.main.py
 from _spy.vitollino.main import Cena
-CENA = "https://i.imgur.com/ucaj8RJ.jpg"
-FOGO = "https://i.imgur.com/FaPZtsh.jpg"
+from random import shuffle
+TEMPLO = "https://i.imgur.com/dQqwYul.jpg"
+TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
+FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
+ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
+COBRA = "https://i.imgur.com/MydpgBT.jpg"
+DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
+MUMIA = "https://i.imgur.com/3215w01.jpg" 
+ARANHA = "http://varg.wdfiles.com/local--files/sr-annals-1/Spiders.jpg"
 
-class Inca:
-      def inicia(self):
-          templo = Cena(CENA)
-          fogo = Cena(FOGO)
-          templo.direita = fogo
-          fogo.esquerda = templo
+templo = Cena(TEMPLO)
+templo.vai()
+tesouro = Cena(TESOURO)
+tesouro.vai()
+fogo = Cena(FOGO)
+fogo.vai()
+artefato1 = Cena(ARTEFATO1)
+artefato1.vai()
+cobra = Cena(COBRA)
+cobra.vai()
+desabamento = Cena(DESABAMENTO)
+desabamento.vai()
+mumia = Cena(MUMIA)
+mumia.vai()
+aranha = Cena(ARANHA)
+aranha.vai()
 
-inca = Inca()
-inca.inicia()
+def decisao (anterior,carta):
+    resposta = input ("Você descobriu uma câmara do templo. Você entra nela ou sai?")
+    if resposta == "entro": 
+        carta.vai() 
+    elif resposta == "saio":
+        anterior.vai()
+
+cartas = ['templo', 'tesouro', 'fogo', 'artefato1', 'cobra', 'desabamento', 'mumia', 'aranha']
+baralho = cartas[:] # Copy cartas
+shuffle(baralho) # Shuffle baralho
+baralho.vai()
+
+# decisao(tesouro, fogo)

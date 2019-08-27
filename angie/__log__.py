8d0b4dc4ce8c4605de95f13f6f19c143e7205e80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:58:09 -0300

    Created angie/__log__.py at Timestamp: 2019-08-27 13:58:09

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 27 2019 10:58:06.231 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  TESOURO = "https://i.imgur.com/nBbQxbD.jpg
+                                            ^
+SyntaxError: EOL while scanning string literal
+'''},

106ffb74b32658ac235aea11e334efeca1465f16
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:56:45 -0300

    Created lisa/__log__.py at Timestamp: 2019-08-27 13:56:45

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 27 2019 10:56:43.815 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module stacy.main line 5
+  ARTEFATO = 
+              ^
+SyntaxError: invalid syntax
+'''},

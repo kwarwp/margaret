ab74b12cf629117906f440712c65d77a5443ec1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 11:11:48 -0300

    Saved kathryn/main.py at Timestamp: 2019-08-27 14:11:47

--- kathryn/main.py
+++ kathryn/main.py
@@ -3,6 +3,7 @@ from _spy.vitollino.main import Cena
 CENA = "https://i.imgur.com/yPBlUJc.jpg"
 TESOURO = "https://i.imgur.com/DsncQRP.jpg"
 MUMIA = "https://i.imgur.com/ardMlqr.jpg"
+FOGO = 
 templo = Cena(CENA)
 tesouro = Cena(TESOURO) 
 mumia = Cena(MUMIA)

48340f68b9cea5cf87c3114d3c741557c644d469
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 10:56:43 -0300

    Saved libby/main.py at Timestamp: 2019-08-27 13:56:43

--- libby/main.py
+++ libby/main.py
@@ -1,9 +1,11 @@
 # margaret.libby.main.py
 from _spy.vitollino.main import Cena 
+from stacy.main import fogo
 CENA = "https://i.imgur.com/q4Cznxt.jpg"
 TESOURO = "https://i.imgur.com/x2wLF9U.jpg"
 templo = Cena(CENA)
 tesouro = Cena(TESOURO)
 templo.direita = tesouro
 tesouro.esquerda = templo
+tesouro.direita = fogo
 templo.vai()

50433d60395a0d18f110503be648545e2ed72c51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-29 09:35:43 -0300

    Saved stacy/__log__.py at Timestamp: 2019-08-29 12:35:42

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -343,3 +343,19 @@ AttributeError: 'str' object has no attribute 'vai'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Aug 29 2019 09:35:42.982 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    baralho.esquerda = templo
+AttributeError: 'list' object has no attribute 'esquerda'
+'''},

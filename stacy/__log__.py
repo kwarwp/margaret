d7515d54d526eb15e11c2bdfb17a889a030a981a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-29 09:28:33 -0300

    Saved stacy/__log__.py at Timestamp: 2019-08-29 12:28:33

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -192,3 +192,19 @@ AttributeError: 'list' object has no attribute 'esquerda'
     cartas.esquerda = templo
 AttributeError: 'list' object has no attribute 'esquerda'
 '''},
+{'date': 'Thu Aug 29 2019 09:28:33.596 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    cartas.esquerda = templo
+AttributeError: 'list' object has no attribute 'esquerda'
+'''},

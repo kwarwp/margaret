943ba6dda8d0de0760617a615f2b2a68178495a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:29:15 -0300

    Saved meredith/main.py at Timestamp: 2019-09-03 12:29:15

--- meredith/main.py
+++ meredith/main.py
@@ -1,13 +1,16 @@
 # margaret.meredith.main.py
 from _spy.vitollino.main import Cena
 from stacy.main import fogo
-CENA = "https://i.imgur.com/7GZetDn.jpg"
-TESOURO = "https://i.imgur.com/h8MfuRD.jpg"
+
+CARTAS = {
+    'TEMPLO': "https://i.imgur.com/7GZetDn.jpg",
+    'TESOURO': "https://i.imgur.com/h8MfuRD.jpg",
+}
 
 class Inca:
     def inicia(self):
-        templo = Cena(CENA)
-        tesouro = Cena(TESOURO)
+        templo = Cena(CARTAS['TEMPLO'])
+        tesouro = Cena(CARTAS['TESOURO'])
         templo.direita = tesouro
         tesouro.esquerda = templo
         tesouro.direita = fogo
@@ -15,5 +18,6 @@ class Inca:
         templo.vai()
 
 inca = Inca()
-inca.inicia()
-    
+
+if __name__ == "__main__":
+    inca.inicia()    

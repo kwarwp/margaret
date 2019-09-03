8fc14c46463694a093ec0eb6e2f4bcff7ef51874
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:34:29 -0300

    Created tracy/__log__.py at Timestamp: 2019-09-03 12:34:29

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 03 2019 09:34:31.27 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  templo = Cena(DI["CENA"]
+                                    ^
+SyntaxError: invalid syntax
+'''},

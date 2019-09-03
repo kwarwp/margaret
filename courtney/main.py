566e0a87e012cff5202264fa17e7e2a6e15d98f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 08:59:43 -0300

    Saved courtney/main.py at Timestamp: 2019-09-03 11:59:42

--- courtney/main.py
+++ courtney/main.py
@@ -1,4 +1,18 @@
-# margaret.courtney.main.py
-from _spy.vitollino.main import Cena
-CENA = "https://i.imgur.com/XOlPRhQ.jpg"
-ARTEFATO = "https://i.imgur.com/zIYXEyt.jpg"
+# margaret.kathryn.main.py
+from _spy.vitollino.main import Cena 
+CENA = "https://i.imgur.com/IdXGTYa.jpg"
+TESOURO = "https://i.imgur.com/78iHROq.jpg"
+MONSTRO = "https://i.imgur.com/7G66ctJ.png"
+
+class Inca:
+def inicia (self):
+templo = Cena(CENA)
+tesouro = Cena(TESOURO) 
+mumia = Cena(MUMIA)
+templo.direita = tesouro 
+tesouro.esquerda = templo 
+templo.vai()
+tesouro.direita = mumia 
+mumia.esquerda = tesouro 
+mumia.vai()
+

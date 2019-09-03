b3ad3f7599036f4239b9798e53b7f1e18726e510
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:19:40 -0300

    Saved libby/main.py at Timestamp: 2019-09-03 13:19:39

--- libby/main.py
+++ libby/main.py
@@ -1,22 +1,43 @@
 # margaret.libby.main.py
+#__author__ = "Ana Voig frade.giovana13@gmail.com"
 from _spy.vitollino.main import Cena 
+#from stacy.main import fogo
 #IMAGENS = ["CENA", "TESOURO"]
 DIC = DICIONARIO_DE_IMAGENS = {}
-DIC["CENA"] = "https://i.imgur.com/q4Cznxt.jpg"
+DIC["TEMPLO"] = "https://i.imgur.com/q4Cznxt.jpg"
 DIC["TESOURO"] = "https://i.imgur.com/x2wLF9U.jpg"
+DIC["DEGRAU"] = "https://i.imgur.com/UxV1LyG.jpg"
+DIC["ARTEFATO1"] = "https://i.imgur.com/agqn9Gg.jpg"
 
 class Inca:
     def inicia(self):
-        templo = Cena(DIC["CENA"])
+        templo = Cena(DIC["TEMPLO"])
         tesouro = Cena(DIC["TESOURO"])
-        templo.direita = tesouro
+        degrau = Cena(DIC["DEGRAU"])
+        artefato01 = Cena(DIC["ARTEFATO01"])
+        templo.direita = degrau
+        degrau.direita = artefato01
         tesouro.esquerda = templo
-        tesouro.direita = fogo
-        fogo.esquerda = tesouro
+        #tesouro.direita = fogo
+        #fogo.esquerda = tesouro
         tesouro.esquerda = templo
         templo.vai()
+        
+#outro jeito mais sofisticado:
+        
+class Carta: 
+    pass
+    def baralho(self):
+        return []
+        
+class Jogo:
+    def __init__(self):
+        self.baralho = Carta().baralho()
+        self.templo = Cena(DIC["TEMPLO"])
+    def inicia(self):
+        pass
     
-inca = Inca()
+inca = Jogo() # Inca()
  
 if __name__ == "__main__":
-    inicia.inicia()
+    inca.inicia()

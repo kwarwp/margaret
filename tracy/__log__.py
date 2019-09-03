e4944b7ebc0f323ded60630a3d01bdc5897a7585
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:40:56 -0300

    Saved tracy/__log__.py at Timestamp: 2019-09-03 12:40:55

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -81,3 +81,21 @@ KeyError: fogo
     fogo = Cena(DI["fogo"])
 KeyError: fogo
 '''},
+{'date': 'Tue Sep 03 2019 09:40:58.457 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    inca.inicia()
+  module <module> line 16
+    templo.direita = tesouro
+NameError: name 'templo' is not defined
+'''},

24e8125ed142078a922db3adcead6e81d56cfcb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:40:41 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-03 12:40:41

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 03 2019 09:40:40.755 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  if__name__ == "__main__":
+                           ^
+SyntaxError: invalid syntax
+'''},

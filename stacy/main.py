cac32b04fe1efb636e585b58ccafefc0c1983873
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-02 21:23:29 -0300

    Saved stacy/main.py at Timestamp: 2019-09-03 00:23:29

--- stacy/main.py
+++ stacy/main.py
@@ -1,36 +1,11 @@
 # margaret.stacy.main.py
 from _spy.vitollino.main import Cena
-from random import shuffle
-TEMPLO = "https://i.imgur.com/dQqwYul.jpg"
+TEMPLO = "https://i.imgur.com/LXptu0U.jpg"
 TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
-FOGO = "https://i.imgur.com/A6Z2zqB.jpg"
+FOGO = "https://i.imgur.com/KRK66bR.jpg"
 ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
 COBRA = "https://i.imgur.com/MydpgBT.jpg"
 DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
-MUMIA = "https://i.imgur.com/3215w01.jpg" 
-ARANHA = "http://varg.wdfiles.com/local--files/sr-annals-1/Spiders.jpg"
+MUMIA = "https://i.imgur.com/HPp1k5T.jpg" 
+ARANHA = "https://i.imgur.com/w90m1jf.jpg"
 
-templo = Cena(TEMPLO)
-templo.vai()
-tesouro = Cena(TESOURO)
-fogo = Cena(FOGO)
-artefato1 = Cena(ARTEFATO1)
-cobra = Cena(COBRA)
-desabamento = Cena(DESABAMENTO)
-mumia = Cena(MUMIA)
-aranha = Cena(ARANHA)
-
-cartas = [TESOURO(), ARTEFATO1(), FOGO(), MUMIA(), DESABAMENTO(), ARANHA(), COBRA()]
-baralho = cartas[:] # Copy cartas
-shuffle(baralho) # Shuffle baralho
-for elemento in cartas:
-    elemento.vai()
-
-def decisao (anterior,carta):
-    resposta = input ("Você descobriu uma câmara do templo. Você entra nela ou sai?")
-    if resposta == "entro": 
-        carta.vai() 
-    elif resposta == "saio":
-        anterior.vai()
-
-# decisao(tesouro, fogo)

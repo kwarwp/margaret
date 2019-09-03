c1d8d0fd0abc01d3629e3a798f6d883eee9ad229
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 11:01:29 -0300

    Saved angie/__log__.py at Timestamp: 2019-09-03 14:01:28

--- angie/__log__.py
+++ angie/__log__.py
@@ -159,3 +159,21 @@ IndexError: list index out of range
     self.templo.direita = self.baralho[4]
 IndexError: list index out of range
 '''},
+{'date': 'Tue Sep 03 2019 11:01:21.10 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    inca = Jogo()  # Inca()
+  module <module> line 21
+    self.templo.direita = self.baralho[4]
+IndexError: list index out of range
+'''},

b532142687d3e4771e7f42cfbb4630fbd42a6da9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 08:56:40 -0300

    Saved angie/main.py at Timestamp: 2019-09-03 11:56:39

--- angie/main.py
+++ angie/main.py
@@ -4,17 +4,20 @@ CENA = "https://i.imgur.com/6NNu6ps.jpg"
 TESOURO = "https://i.imgur.com/nBbQxbD.jpg"
 COBRA = "https://i.imgur.com/0paGPPh.jpg"
 FOGO = "https://i.imgur.com/ScR01C1.png"
-templo = Cena (CENA).vai()
-tesouro = Cena(TESOURO).vai()
-cobra = Cena (COBRA).vai()
-fogo = Cena (FOGO).vai()
-templo.direita = tesouro
-templo.vai()
-tesouro.esquerda = templo
-tesouro.direita = cobra
-cobra.esquerda = tesouro
-cobra.direita = fogo
-fogo.esquerda = cobra 
+
+class Inca:
+    def inicia(self):
+        templo = Cena (CENA).vai()
+        tesouro = Cena(TESOURO).vai()
+        cobra = Cena (COBRA).vai()
+        fogo = Cena (FOGO).vai()
+        templo.direita = tesouro
+        templo.vai()
+        tesouro.esquerda = templo
+        tesouro.direita = cobra
+        cobra.esquerda = tesouro
+        cobra.direita = fogo
+        fogo.esquerda = cobra 
 
 inca = Inca()
 inca.inicia()

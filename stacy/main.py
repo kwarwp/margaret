5f75e13a1138e5be0d1ee778672e7f2d4a6e73ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-02 21:30:41 -0300

    Saved stacy/main.py at Timestamp: 2019-09-03 00:30:40

--- stacy/main.py
+++ stacy/main.py
@@ -8,4 +8,12 @@ COBRA = "https://i.imgur.com/MydpgBT.jpg"
 DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
 MUMIA = "https://i.imgur.com/HPp1k5T.jpg" 
 ARANHA = "https://i.imgur.com/w90m1jf.jpg"
+templo = Cena(TEMPLO)
+tesouro = Cena(TESOURO)
+fogo = Cena(FOGO)
+artefato1 = Cena(ARTEFATO1)
+cobra = Cena(COBRA)
+desabamento = Cena(DESABAMENTO)
+mumia = Cena(MUMIA)
+aranha = Cena(ARANHA)
 

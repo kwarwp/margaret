8b65bc4de7468b7335ba287dac6374562f3281f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:57:37 -0300

    Saved lisa/main.py at Timestamp: 2019-09-03 13:57:35

--- lisa/main.py
+++ lisa/main.py
@@ -1,28 +1,19 @@
 # margaret.lisa.main.py
 __author__ = "Carlo E. T. Oliveira carlo@ufrj.br"
 from _spy.vitollino.main import Cena
+from ruzwana.main import DI as RDI
 #from stacy.main import fogo
 #from natalia.main import sereiamonstro
-#IMAGENS = ["CENA", "TESOURO"]
+IMAGENS = ["FOGO", "PEDRAS", "ARANHA"]
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["TEMPLO"] = "https://i.imgur.com/7GZetDn.jpg"
 DI["TESOURO"] = "https://i.imgur.com/h8MfuRD.jpg"
-
-class Inca:
-    def inicia(self):
-        templo = Cena(DI["TEMPLO"])
-        tesouro = Cena(DI["TESOURO"])
-        templo.direita = tesouro
-        tesouro.esquerda = templo
-        #tesouro.direita = fogo
-        #fogo.esquerda = tesouro
-        templo.vai()
         
 class Carta:
     def __init__(self):
-        self.carta = Cena(DI["TESOURO"])
+        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
     def baralho(self):
-        return [self.carta]
+        return self.cartas
         
 class Jogo:
     def __init__(self):

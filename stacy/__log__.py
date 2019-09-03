21233cf1c373a7818b4a8dca7273e741dd85049f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 11:14:40 -0300

    Saved stacy/__log__.py at Timestamp: 2019-09-03 14:14:40

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1174,3 +1174,23 @@ AttributeError: 'Cartas' object has no attribute 'cartas'
     for ordem, carta in enumerate(self.cartas):
 AttributeError: 'Cartas' object has no attribute 'cartas'
 '''},
+{'date': 'Tue Sep 03 2019 11:14:35.241 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    inca = Jogo()
+  module <module> line 58
+    self.baralho = Cartas().baralho()
+  module <module> line 54
+    return self.carta
+AttributeError: 'Cartas' object has no attribute 'carta'
+'''},

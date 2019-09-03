3996a3ee2604c4de0aa4d1ce19fb126b057dfd3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:07:47 -0300

    Saved stacy/__log__.py at Timestamp: 2019-09-03 12:07:46

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -813,3 +813,42 @@ TypeError: 'str' object is not callable
     cartas = [TESOURO(), ARTEFATO1(), FOGO(), MUMIA(), DESABAMENTO(), ARANHA(), COBRA()]
 TypeError: 'str' object is not callable
 '''},
+{'date': 'Tue Sep 03 2019 09:07:40.756 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from lisa.main import inca
+  module lisa.main line 3
+    from stacy.main import fogo
+  module stacy.main line 3
+    from lisa.main import inca
+ImportError: cannot import name 'inca'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from lisa.main import inca
+  module lisa.main line 3
+    from stacy.main import fogo
+  module stacy.main line 3
+    from lisa.main import inca
+'''},

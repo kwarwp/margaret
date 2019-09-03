e761bef2f7715aec6796a9815c2029dbe6651e19
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:01:37 -0300

    Saved courtney/__log__.py at Timestamp: 2019-09-03 12:01:36

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -154,3 +154,10 @@ NameError: name 'mostro' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 03 2019 09:01:39.738 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  def inicia (self):
+  ^
+IndentationError: expected an indented block
+'''},

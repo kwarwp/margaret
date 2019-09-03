63eec56bb9bfa495a61ee50cf32ea888d199cefe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 11:14:37 -0300

    Saved stacy/main.py at Timestamp: 2019-09-03 14:14:37

--- stacy/main.py
+++ stacy/main.py
@@ -46,7 +46,7 @@ class Inca:
     
 class Cartas:
     def __init__(self):
-        self.carta = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS]
+        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS]
         for ordem, carta in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
                 carta.direita = self.cartas[ordem+1]

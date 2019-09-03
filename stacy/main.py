349b9de0ca48fb6fe94074be2640eb318589253d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:59:04 -0300

    Saved stacy/main.py at Timestamp: 2019-09-03 13:59:03

--- stacy/main.py
+++ stacy/main.py
@@ -1,7 +1,7 @@
 # margaret.stacy.main.py
 __author__ = "Victória Regina Caruzo victorialourencocaruzo@gmail.com"
 from _spy.vitollino.main import Cena
-IMAGENS = ["TEMPLO", "TESOURO", "FOGO", "ARTEFATO1", "COBRA", "DESABAMENTO", "MUMIA", "ARANHA"]
+IMAGENS = ["TESOURO", "FOGO", "ARTEFATO1", "COBRA", "DESABAMENTO", "MUMIA", "ARANHA"]
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["TEMPLO"] = "https://i.imgur.com/LXptu0U.jpg"
 DI["TESOURO"] = "https://i.imgur.com/Nq1hCeU.jpg"

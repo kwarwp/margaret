5714b047b95a8284aa3b646d818a5c3780268621
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:22:31 -0300

    Saved naomi/main.py at Timestamp: 2019-09-03 13:22:31

--- naomi/main.py
+++ naomi/main.py
@@ -2,20 +2,41 @@
 from _spy.vitollino.main import Cena
 IMAGENS = ["CENA", "TESOURO"]
 DI = DICIONARIO_DE_IMAGENS = {}
-DI["CENA"] = "https://i.imgur.com/inzdUaq.jpg"
+DI["TEMPLO"] = "https://i.imgur.com/inzdUaq.jpg"
 DI["TESOURO"] = "https://i.imgur.com/DJNXWXY.jpg"
-
+DI["FOGO"] = " https://i.imgur.com/yufZlvN.jpg"
+DI["DESMORONAMENTO"] = "https://i.imgur.com/uMSX7Ka.jpg"
 class Inca:
     def inicia(self):
-        templo = Cena(DI["CENA"])
+        templo = Cena(DI["TEMPLO"])
         tesouro = Cena(DI["TESOURO"])
+        fogo = Cena(DI["FOGO"])
+        desmoronamento = Cena(DI["DESMORONAMENTO"])
         templo.direita = tesouro
         tesouro.esquerda = templo
+        tesouro.direita = fogo
+        fogo.esquerda = tesouro
+        fogo.direita = desmoronamento
+        desmoronamento.esquerda = fogo
         templo.vai()
+        
+class Carta:
+    pass
+    def baralho(self):
+        return []
+        
+class Jogo:
+    def __init__(self):
+        self.baralho = Carta().baralho()
+        self.templo = Cena(DI["TEMPLO"])
+    def inicia(self):
     
-inca = Inca()
+    
+inca = Jogo()  # Inca()
 
 if __name__ == "__main__":
     inca.inicia() 
+    
+    
 
    

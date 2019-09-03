e6e771c2d215d98955fc2409e92ea4edff97b501
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:19:43 -0300

    Saved kellee/main.py at Timestamp: 2019-09-03 13:19:42

--- kellee/main.py
+++ kellee/main.py
@@ -3,7 +3,7 @@ __author__ = "Bárbara Juliana C.Soares barbara_ju07@hotmail.com"
 from _spy.vitollino.main import Cena
 IMAGENS = ["CENA", "TESOURO", "MONSTRO", "FOGO", "PEDRAS"]
 DI = DICIONARIO_DE_IMAGENS = {}
-DI["CENA"] = "https://i.imgur.com/F0pWLRM.png"
+DI["TEMPLO"] = "https://i.imgur.com/F0pWLRM.png"
 DI["TESOURO"] = "https://i.imgur.com/Z2CD83L.jpg"
 DI["MONSTRO"] = "https://i.imgur.com/4cBiZ6n.jpg"
 DI["FOGO"] = "https://i.imgur.com/qLYrtR0.jpg"
@@ -11,7 +11,7 @@ DI["PEDRAS"] = "https://i.imgur.com/rpIsgaD.png"
 
 class Inca:
     def inicia(self):
-        templo = Cena(DI["CENA"])
+        templo = Cena(DI["TEMPLO"])
         tesouro = Cena(DI["TESOURO"])
         monstro = Cena(DI["MONSTRO"])
         fogo = Cena(DI["FOGO"])
@@ -24,7 +24,21 @@ class Inca:
         pedras.esquerda = templo
         templo.vai()
     
-inca = Inca()
+class Carta:
+    pass
+    def baralho(self):
+        return []
+        
+
+class Jogos:
+    def __init__(self)
+        self.baralho = carta().baralho()
+        self.templo = Cena(DI["TEMPLO"])
+    def inicia(self):
+        pass
+        
+        
+inca = Jogo() #  Inca()
 
 if __name__ == "__main__":
     inca.inicia()

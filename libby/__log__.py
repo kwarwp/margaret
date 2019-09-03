e2b6046663c0c8d61e427e77285fed8a68d9a329
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:45:02 -0300

    Saved libby/__log__.py at Timestamp: 2019-09-03 12:45:02

--- libby/__log__.py
+++ libby/__log__.py
@@ -90,3 +90,21 @@ ImportError: cannot import name 'fogo'
     inicia.inicia()
 NameError: name 'inicia' is not defined
 '''},
+{'date': 'Tue Sep 03 2019 09:45:14.355 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    inca.inicia()
+  module <module> line 15
+    tesouro.direita = fogo
+NameError: name 'fogo' is not defined
+'''},

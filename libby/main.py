c3f140ed57fc5de8045d5fd65a2881c3d6eb9964
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:31:56 -0300

    Saved libby/main.py at Timestamp: 2019-09-03 12:31:55

--- libby/main.py
+++ libby/main.py
@@ -1,13 +1,23 @@
 # margaret.libby.main.py
 from _spy.vitollino.main import Cena 
 from stacy.main import fogo
-CENA = "https://i.imgur.com/q4Cznxt.jpg"
-TESOURO = "https://i.imgur.com/x2wLF9U.jpg"
-templo = Cena(CENA)
-tesouro = Cena(TESOURO)
-templo.direita = tesouro
-tesouro.esquerda = templo
-tesouro.direita = fogo
-fogo.esquerda = tesouro
-tesouro.esquerda = templo
-templo.vai()
+#IMAGENS = ["CENA", "TESOURO"]
+DIC = DICIONARIO_DE_IMAGENS = {}
+DIC["CENA"] = "https://i.imgur.com/q4Cznxt.jpg"
+DIC["TESOURO"] = "https://i.imgur.com/x2wLF9U.jpg"
+
+class Inca:
+    def inicia(self):
+        templo = Cena(DIC["CENA"])
+        tesouro = Cena(DIC["TESOURO"])
+        templo.direita = tesouro
+        tesouro.esquerda = templo
+        tesouro.direita = fogo
+        fogo.esquerda = tesouro
+        tesouro.esquerda = templo
+        templo.vai()
+    
+inca = Inca()
+ 
+if __name__ == "__main__":
+     inicia.inicia()

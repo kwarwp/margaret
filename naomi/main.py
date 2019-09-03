904b5490f585f0d1316310741e9f332c49acda74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:47:17 -0300

    Saved naomi/main.py at Timestamp: 2019-09-03 12:47:17

--- naomi/main.py
+++ naomi/main.py
@@ -1,4 +1,23 @@
 # margaret.naomi.main.py
 from _spy.vitollino.main import Cena
 from stacy.main import fogo
-CENA = 
+IMAGENS = ["CENA", "TESOURO"]
+DI = DICIONARIO_DE_IMAGENS = {}
+DI["CENA"] = "https://i.imgur.com/inzdUaq.jpg"
+DI["TESOURO"] = "https://i.imgur.com/DJNXWXY.jpg"
+
+class Inca:
+    def inicia(self):
+        templo = Cena(DI["CENA"])
+        tesouro = Cena(DI["TESOURO"])
+        templo.dieita = tesouro
+        tesouro.esquerda = templo
+        tesouro.direita = fogo
+        fogo.esquerda = tesouro
+        templo.vai()
+    
+inca = Inca()
+
+if__name__ == "__main__":
+   inca.inicia() 
+   

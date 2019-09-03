1782fcb221988acf58227e174c40e792543bf56e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:12:37 -0300

    Saved angie/main.py at Timestamp: 2019-09-03 13:12:37

--- angie/main.py
+++ angie/main.py
@@ -2,15 +2,15 @@
 from _spy.vitollino.main import Cena
 IMAGENS = ["CENA", "TESOURO", "COBRA", "FOGO"]
 DI = DICIONARIO_DE_IMAGENS = {}
-DI["CENA"] = "https://i.imgur.com/6NNu6ps.jpg"
+DI["TEMPLO"] = "https://i.imgur.com/6NNu6ps.jpg"
 DI["TESOURO"] = "https://i.imgur.com/nBbQxbD.jpg"
-DI["COBRA"] = "https://i.imgur.com/0paGPPh.jpg"
+DI["COBRA"] = "https://i.imgur.com/Lh1O87Q.jpg"
 DI["FOGO"] = "https://i.imgur.com/ScR01C1.png"
-DI["MONSTRO"] = "https://i.imgur.com/eb88QC6.jpg"
+DI["MONSTRO"] = "https://i.imgur.com/nvWMlWx.jpg"
 
 class Inca:
     def inicia(self):
-        templo = Cena (DI["CENA"])
+        templo = Cena (DI["TEMPLO"])
         tesouro = Cena (DI["TESOURO"])
         cobra = Cena (DI["COBRA"])
         fogo = Cena (DI["FOGO"])
@@ -25,7 +25,15 @@ class Inca:
         monstro.esquerda = fogo
         templo.vai()
 
+class Carta:
+    pass
+    def baralho(self)
+        return []
+    
 class Jogo:
+    def __init__(self)
+        self.baralho = Carta().baralho()
+        self.templo = Cena(DI["TEMPLO"])
     pass
 
 inca = Inca()

ee071ff24fe73f2bd8c8a5096b3c6d299fa729c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 10:58:16 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-03 13:58:16

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -135,3 +135,23 @@ KeyError: TEMPLO
     self.carta = Cena(DI["TESOURO"])
 KeyError: TESOURO
 '''},
+{'date': 'Tue Sep 03 2019 10:58:15.986 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    inca = Jogo()  # Inca()
+  module <module> line 35
+    self.baralho = Carta().baralho()
+  module <module> line 29
+    self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
+NameError: name 'RDI' is not defined
+'''},

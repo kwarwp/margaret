b68f1947ed5dbbd84bdc9dff0b66c44f1fc60736
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 11:01:17 -0300

    Saved kellee/main.py at Timestamp: 2019-09-03 14:01:16

--- kellee/main.py
+++ kellee/main.py
@@ -8,25 +8,10 @@ DI["TESOURO"] = "https://i.imgur.com/Z2CD83L.jpg"
 DI["MONSTRO"] = "https://i.imgur.com/4cBiZ6n.jpg"
 DI["FOGO"] = "https://i.imgur.com/qLYrtR0.jpg"
 DI["PEDRAS"] = "https://i.imgur.com/rpIsgaD"
-
-class Inca:
-    def inicia(self):
-        templo = Cena(DI["CENA"])
-        tesouro = Cena(DI["TESOURO"])
-        monstro = Cena(DI["MONSTRO"])
-        fogo = Cena(DI["FOGO"])
-        pedras = Cena(DI["PEDRAS"])
-        templo.direita = tesouro
-        tesouro.esquerda = templo
-        tesouro.direita = monstro
-        monstro.esquerda = fogo
-        fogo.direita = pedras 
-        pedras.esquerda = templo
-        templo.vai()
     
 class Carta:
     def __init__(self):
-        self.carta = Cena(DI[uma_imagem]) for uma_imagem in IMAGENS] 
+        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS] 
     def baralho(self):
         return self.cartas
         
@@ -35,7 +20,7 @@ class Jogo:
     def __init__(self):
         self.baralho = Carta().baralho()
         self.templo = Cena(DI["CENA"])
-        self.templo.direita = self.baralho[0]
+        self.templo.direita = self.baralho[1]
     def inicia(self):
         self.templo.vai()
         

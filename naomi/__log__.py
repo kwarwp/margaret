b95affe9d2e0936bc634bbbf6e7a437a7b2674e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:38:56 -0300

    Created naomi/__log__.py at Timestamp: 2019-09-03 12:38:55

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 03 2019 09:38:53.634 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  templo = Cena(DI["CENA"])
+  ^
+IndentationError: expected an indented block
+'''},

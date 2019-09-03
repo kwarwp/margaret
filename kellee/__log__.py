c87d84526844b5788ec7b08a702788ff422a046a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:38:58 -0300

    Saved kellee/__log__.py at Timestamp: 2019-09-03 12:38:57

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -36,3 +36,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Sep 02 2019 16:48:31.74 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  if __name__ == "__main__":
+                            ^
+IndentationError: expected an indented block
+'''},

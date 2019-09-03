eae4d42037a870093c9b12bc268b9bddc29b5554
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:25:17 -0300

    Saved stacy/main.py at Timestamp: 2019-09-03 12:25:16

--- stacy/main.py
+++ stacy/main.py
@@ -1,15 +1,22 @@
 # margaret.stacy.main.py
 from _spy.vitollino.main import Cena
-TEMPLO = "https://i.imgur.com/LXptu0U.jpg"
-TESOURO = "https://i.imgur.com/Nq1hCeU.jpg"
-FOGO = "https://i.imgur.com/KRK66bR.jpg"
-ARTEFATO1 = "https://i.imgur.com/1QHNdyI.jpg"
-COBRA = "https://i.imgur.com/MydpgBT.jpg"
-DESABAMENTO = "https://i.imgur.com/jnxWklS.jpg"
-MUMIA = "https://i.imgur.com/HPp1k5T.jpg" 
-ARANHA = "https://i.imgur.com/w90m1jf.jpg"
-templo = Cena(TEMPLO)
-tesouro = Cena(TESOURO)
+IMAGENS = ["TEMPLO", "TESOURO", "FOGO", "ARTEFATO1", "COBRA", "DESABAMENTO", "MUMIA", "ARANHA"]
+DI = DICIONARIO_DE_IMAGENS = {}
+DI["TEMPLO"] = "https://i.imgur.com/LXptu0U.jpg"
+DI["TESOURO"] = "https://i.imgur.com/Nq1hCeU.jpg"
+DI["FOGO"] = "https://i.imgur.com/KRK66bR.jpg"
+DI["ARTEFATO1"] = "https://i.imgur.com/1QHNdyI.jpg"
+DI["COBRA"] = "https://i.imgur.com/MydpgBT.jpg"
+DI["DESABAMENTO"] = "https://i.imgur.com/jnxWklS.jpg"
+DI["MUMIA"] = "https://i.imgur.com/HPp1k5T.jpg" 
+DI["ARANHA"] = "https://i.imgur.com/w90m1jf.jpg"
+
+class Inca:
+    def inicia(self):
+        templo = Cena(DI["TEMPLO"])
+        tesouro = Cena(TESOURO)
+
+
 fogo = Cena(FOGO)
 artefato1 = Cena(ARTEFATO1)
 cobra = Cena(COBRA)

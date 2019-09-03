8c1695093852dda95fa666cccea76d1a16d2b685
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 11:19:28 -0300

    Saved angie/main.py at Timestamp: 2019-09-03 14:19:28

--- angie/main.py
+++ angie/main.py
@@ -1,7 +1,7 @@
 # margaret.angie.main.py
 from _spy.vitollino.main import Cena
 from random import shuffle
-IMAGENS = ["CENA", "TESOURO", "COBRA", "FOGO", "MONSTRO"]*5
+IMAGENS = ["CENA", "TESOURO", "COBRA", "FOGO", "MONSTRO", "PEDRAS"]*5
 shuffle(IMAGENS)
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["CENA"] = "https://i.imgur.com/6NNu6ps.jpg"
@@ -9,7 +9,7 @@ DI["TESOURO"] = "https://i.imgur.com/nBbQxbD.jpg"
 DI["COBRA"] = "https://i.imgur.com/Lh1O87Q.jpg"
 DI["FOGO"] = "https://i.imgur.com/ScR01C1.png"
 DI["MONSTRO"] = "https://i.imgur.com/nvWMlWx.jpg"
-
+DI["PEDRAS"] = "https://i.imgur.com/gWFNSS2.jpg"
 class Carta:
     def __init__(self):
         self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS]

9ae4be500fc5db8d12438f72e83a2f3a7f325b6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 09:10:29 -0300

    Saved courtney/__log__.py at Timestamp: 2019-09-03 12:10:29

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -186,3 +186,21 @@ IndentationError: expected an indented block
     cobra = Cena (COBRA).vai()
 NameError: name 'COBRA' is not defined
 '''},
+{'date': 'Tue Sep 03 2019 09:10:32.106 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    inca.inicia()
+  module <module> line 12
+    cobra = Cena (COBRA).vai()
+NameError: name 'COBRA' is not defined
+'''},

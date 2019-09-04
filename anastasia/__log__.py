3e695fcec0409907ba43ca892e8b9a2248b5d223
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:28:34 -0300

    Saved anastasia/__log__.py at Timestamp: 2019-09-04 14:28:34

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -54,3 +54,19 @@ NameError: name 'tempo' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 04 2019 11:28:33.122 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    jogo = jogo()
+NameError: name 'jogo' is not defined
+'''},

6976d0f147a26a494fb45ec739600e5396834bce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:39:00 -0300

    Created kristen/main.py at Timestamp: 2019-09-04 14:39:00

---
+++ kristen/main.py
@@ -0,0 +1,22 @@
+# margaret.kristen.main.py
+from _spy.vitollino.main import Cena
+TEMPLO = "https://i.imgur.com/hy9jUf5.jpg"
+TESOURO = "https://i.imgur.com/pcrB1gm.jpg"
+PERIGOS = {
+    "MÚMIA": "https://i.imgur.com/vaRhKqK.jpg",
+    "COBRA": "https://i.imgur.com/oTs2Tf4.jpg",
+    "DESMORONAMENTO": "https://i.imgur.com/gNFtoMy.jpg",
+    "ARANHA": "https://i.imgur.com/gNFtoMy.jpg",
+    "FOGO": "https://i.imgur.com/4XA6XhB.jpg",
+} 
+class Jogo: 
+    def inicia(self):
+        templo = Cena (TEMPLO)
+        tesouro = Cena (TESOURO)
+        templo.direita = tesouro
+        tesouro.esquerda = templo 
+        mumia = Cena (PERIGOS["mumia"])
+        templo.vai()
+        
+jogo = Jogo()
+jogo.inicia()

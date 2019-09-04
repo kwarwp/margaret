f88d5c3354392f35ef767b077e4e0adeb95eb935
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 12:28:09 -0300

    Saved sarah/__log__.py at Timestamp: 2019-09-04 15:28:09

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -6,3 +6,10 @@
             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Sep 04 2019 12:28:09.192 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  def __init__(self) #dunder
+                             ^
+SyntaxError: invalid syntax
+'''},

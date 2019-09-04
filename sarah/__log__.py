2666b2a8b2407d502e1bfe6a31cb4fe2896ca507
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 12:32:45 -0300

    Saved sarah/__log__.py at Timestamp: 2019-09-04 15:32:45

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -34,3 +34,21 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 04 2019 12:32:44.672 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    jogo.inicia()
+  module <module> line 50
+    tesouro.direita = mumia
+NameError: name 'mumia' is not defined
+'''},

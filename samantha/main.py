7294f5300896f3a9f81bce7a2ccb5759d311c990
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:16:40 -0300

    Created samantha/main.py at Timestamp: 2019-09-04 14:16:39

---
+++ samantha/main.py
@@ -0,0 +1,9 @@
+# margaret.samantha.main.py
+from _spy.vitollino.main import Cena
+TEMPLO = "https://i.imgur.com/Gr3jyiF.jpg"
+TESOURO = "https://i.imgur.com/ykncQGm.jpg"
+
+
+templo = Cena(TEMPLO)
+templo.vai()
+

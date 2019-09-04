3eabfebcd13b9504a13e4029fa8471beeb5f22c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:18:17 -0300

    Saved sarah/main.py at Timestamp: 2019-09-04 14:18:16

--- sarah/main.py
+++ sarah/main.py
@@ -3,8 +3,6 @@ from _spy.vitollino.main import Cena
 #Quando Classe inicia em maiuscula, quando constante não é maiuscula
 TEMPLO = "https://i.imgur.com/Tsxuzh2.jpg"
 TESOURO = "https://i.imgur.com/pEvCidq.jpg"
-JOGADOR =
-ESCADA = 
 
 templo = Cena(TEMPLO)
 #templo nome de uma instancia de um objeto

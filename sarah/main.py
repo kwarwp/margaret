268b3a3d9d4fbc79c2ed2f7e0c63507f3e1a7fe8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:16:45 -0300

    Created sarah/main.py at Timestamp: 2019-09-04 14:16:45

---
+++ sarah/main.py
@@ -0,0 +1,12 @@
+# margaret.sarah.main.py
+from _spy.vitollino.main import Cena
+#Quando Classe inicia em maiuscula, quando constante não é maiuscula
+TEMPLO = "https://i.imgur.com/Tsxuzh2.jpg"
+TESOURO = "https://i.imgur.com/pEvCidq.jpg"
+JOGADOR =
+ESCADA = 
+
+templo = Cena(TEMPLO)
+#templo nome de uma instancia de um objeto
+#cena é uma instancia de uma classe
+TEMPLO.VAI()

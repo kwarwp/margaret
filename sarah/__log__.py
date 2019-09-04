934710376f282e41adc80501d625b51102e31871
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:17:47 -0300

    Created sarah/__log__.py at Timestamp: 2019-09-04 14:17:47

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 04 2019 11:17:45.666 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  JOGADOR =
+            ^
+SyntaxError: invalid syntax
+'''},

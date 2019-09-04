8522fa140c653c7799dff45b3180cfeed4c1b0b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:26:18 -0300

    Saved sarah/main.py at Timestamp: 2019-09-04 14:26:18

--- sarah/main.py
+++ sarah/main.py
@@ -2,12 +2,15 @@
 from _spy.vitollino.main import Cena
 #Quando Classe inicia em maiuscula, quando constante não é maiuscula
 TEMPLO = "https://i.imgur.com/Tsxuzh2.jpg"
-TESOURO = "https://i.imgur.com/pEvCidq.jpg"
+TESOURO = "https://i.imgur.com/tIsfRPD.jpg"
 
-templo = Cena(TEMPLO)
-#templo nome de uma instancia de um objeto
-#cena é uma instancia de uma classe
-tesouro = Cena(TESOURO)
-templo.direita = tesouro
-tesouro.esquerda = templo
-templo.vai()
+class Jogo:
+    def inicia(self):
+        templo = Cena(TEMPLO)
+        #templo nome de uma instancia de um objeto
+        #cena é uma instancia de uma classe
+        tesouro = Cena(TESOURO)
+        templo.direita = tesouro
+        tesouro.esquerda = templo
+        templo.vai()
+        

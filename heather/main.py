99bfd43ef1b7fabb7b4774b5b495be11d831c785
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 12:25:31 -0300

    Saved heather/main.py at Timestamp: 2019-09-04 15:25:31

--- heather/main.py
+++ heather/main.py
@@ -7,16 +7,31 @@ PERIGOS = {
     "COBRA": ""
 }
 
+class Cartas:
+    def __init__(self):
+        self.baralho = [Cena(perigo) for perigo in PERIGOS.values()]
+        baralho_amanha = self.baralho[1:]
+        baralho_zip = zip(self.baralho, baralho_amanha)
+        for c_hoje, c_ama in baralho_zip:
+            c_hoje.direita = c_ama
+            c_ama.esquerda = c_hoje
+            
+    def primeira_carta(self):
+        return self.baralho[0]
+
+
 class Jogo:
     def inicia(self):
         templo = Cena(TEMPLO)
         tesouro = Cena(TESOURO)
         templo.direta = tesouro
         tesouro.esquerda = templo
-        mumia = Cena(PERIGOS["MUMIA"])
-        tesouro.direita = mumia
-        mumia.esquerda = tesouro
+        perigos = Cartas()
+        umperigo = perigos.primeira_carta()
+        tesouro.direita = umperigo
+        umperigo.esquerda = tesouro
         templo.vai()
         
+            
 jogo = Jogo()
 jogo.inicia()

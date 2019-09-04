e2b7c451272200ac6337d88dc8cea471b569d697
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:37:14 -0300

    Saved samantha/main.py at Timestamp: 2019-09-04 14:37:14

--- samantha/main.py
+++ samantha/main.py
@@ -2,8 +2,26 @@
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/Gr3jyiF.jpg"
 TESOURO = "https://i.imgur.com/ykncQGm.jpg"
+PERIGOS = {
+       "MUMIA": "",
+       "COBRA": ""
+
+}
+
+class Jogo:
+    def inicia(self):
+        templo = Cena(TEMPLO)
+        tesouro = Cena(TESOURO)
+        templo.direita = tesouro
+        tesouro.esquerda = templo
+        mumia = Cena(PERIGOS["MUMIA"])
+        tesouro.direita = mumia
+        mumia.esquerda = tesouro 
+        templo.vai()
+         
+jogo = Jogo()
+jogo.inicia()
 
 
-templo = Cena(TEMPLO)
-templo.vai()
+
 

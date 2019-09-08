4420fad5fbbee24fe28450c9e013abed781c73fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-08 18:44:29 -0300

    Saved libby/main.py at Timestamp: 2019-09-08 21:44:29

--- libby/main.py
+++ libby/main.py
@@ -1,31 +1,23 @@
 # margaret.libby.main.py
 #__author__ = "Ana Voig frade.giovana13@gmail.com"
 from _spy.vitollino.main import Cena 
-from ruzwana.main import DI as RDI
+#from ruzwana.main import DI as RDI
 #from stacy.main import fogo
 IMAGENS = ["FOGO", "PEDRAS", "ARANHA"]
 DIC = DICIONARIO_DE_IMAGENS = {}
 DIC["TEMPLO"] = "https://i.imgur.com/q4Cznxt.jpg"
-DIC["TESOURO"] = "https://i.imgur.com/mZ6E5Qs.jpg"
 DIC["DEGRAU"] = "https://i.imgur.com/UxV1LyG.jpg"
 DIC["ARTEFATO1"] = "https://i.imgur.com/agqn9Gg.jpg"
+DIC["ARTEFATO2"] = "https://i.imgur.com/r3Wkmtv.jpg"
+DIC["ARTEFATO3"] = "https://i.imgur.com/QI7eW76.jpg"
+DIC["ARTEFATO4"] = "https://i.imgur.com/WOU3fek.png"
+DIC["ARTEFATO5"] = "https://i.imgur.com/xe9l1fP.jpg"
+DIC["TESOURO"] = "https://i.imgur.com/mZ6E5Qs.jpg"
+DIC["ARANHA"] = "https://i.imgur.com/tQLsXB0.jpg"
+DIC["MÚMIA"] = "https://i.imgur.com/OVSH3Aa.jpg"
+DIC["FOGO"] = "https://i.imgur.com/LmoJDRb.jpg"
+DIC["DESMORONAMENTO"] = "https://i.imgur.com/Wl29PRv.jpg"
 
-#class Inca:
-    #def inicia(self):
-        #templo = Cena(DIC["TEMPLO"])
-        #tesouro = Cena(DIC["TESOURO"])
-        #degrau = Cena(DIC["DEGRAU"])
-        #artefato01 = Cena(DIC["ARTEFATO01"])
-        #templo.direita = degrau
-        #degrau.direita = artefato01
-        #tesouro.esquerda = templo
-        #tesouro.direita = fogo
-        #fogo.esquerda = tesouro
-        #tesouro.esquerda = templo
-        #templo.vai()
-        
-#outro jeito mais sofisticado:
-        
 class Cartas: 
     def __init__(self):
         self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]

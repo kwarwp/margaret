879ed596f9118d5c2af4de65f26f8c395ad711fd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-09 15:17:57 -0300

    Saved online/__log__.py at Timestamp: 2019-09-09 18:17:57

--- online/__log__.py
+++ online/__log__.py
@@ -36,3 +36,19 @@ SyntaxError: invalid syntax
                       ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Mon Sep 09 2019 15:17:56.379 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    with window.Peer as (pjs, err):
+AttributeError: no attribute Peer for [object Window]
+'''},

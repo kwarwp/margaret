dda850d0805b4e78b67434629f7839cc84d1d38b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-09 00:29:27 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-09 03:29:27

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -283,3 +283,10 @@ SyntaxError: invalid syntax
                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 09 2019 00:29:50.197 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  shuffle(IMAGENS) STYLE["Width"]=600
+                    ^
+SyntaxError: invalid syntax
+'''},

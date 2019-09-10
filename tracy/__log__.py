7af99263534b9210bcb5b5404d9ea04df17d3002
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:40:14 -0300

    Saved tracy/__log__.py at Timestamp: 2019-09-10 13:40:13

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -211,3 +211,23 @@ NameError: name 'RDI' is not defined
     self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
 KeyError: PEDRAS
 '''},
+{'date': 'Tue Sep 10 2019 10:40:15.600 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    inca = Jogo()  # Inca()
+  module <module> line 52
+    self.baralho = Carta().baralho()
+  module <module> line 42
+    self.cartas = [Perigo(DI[uma_imagem],uma_imagem) 
+KeyError: MUMIA
+'''},

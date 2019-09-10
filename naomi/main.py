24974e2d80198c195d5cfa375f2c4776ad8c1055
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:24:29 -0300

    Saved naomi/main.py at Timestamp: 2019-09-10 13:24:29

--- naomi/main.py
+++ naomi/main.py
@@ -4,6 +4,7 @@ from _spy.vitollino.main import Cena
 from random import shuffle
 IMAGENS = ["CENA" , "ARTEFATO" , "FOGO" , "DESMORONAMENTO" , "COBRA" , "PEDRINHAS" , "ARANHA" , "MUMIA" , "SALA DO TESOURO" , "PEPITAS DE OURO" , "OBSIDIANA" , "TURQUESA" , "ACAMPAMENTO"]*5 
 shuffle(IMAGENS) 
+Perigo = {}
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["CENA"] = "https://i.imgur.com/inzdUaq.jpg"
 DI["ARTEFATO"] = "https://i.imgur.com/DJNXWXY.jpg"
@@ -17,28 +18,38 @@ DI["SALA DO TESOURO"] = "https://i.imgur.com/w3DPsMb.jpg"
 DI["PEPITAS DE OURO"] = "https://i.imgur.com/amIQQ8Z.jpg"
 DI["OBSIDIANA"] = "https://i.imgur.com/ShXGOw0.png"
 DI["TURQUESA"] = " "
-DI["ACAMPAMENTO"] = "https://i.imgur.com/MEl27uM.png"
-        
+ACAMPAMENTO = Cena("https://i.imgur.com/MEl27uM.png")
 class Perigo:
     def __init__(self, imagem, tipo):
         self.cena = Cena(imagem)
         self.tipo =tipo
         self.cena_vai = self.cena.vai
         self.cena.vai = self.vai
+        self.acampamento = ACAMPAMENTO
         
+    def set_direita(self, direita):   
+        self.cena.diereita = direita
+        
+    def set_esquerda(self, esquerda):   
+        self.cena.esquerda = esquerda    
     def vai(self):
+        if self.tipo in PERIGOS:
+            #deu ruim, já tinha aparecido um perigo
+            self.cena.direita = self.acampamento
+        else:
+            PERIGOS[self.tipo] = 1
         self.cena_vai()
         
 class CARTA:  
     def __init__(self):
-        self.cartas  [Cena(DI[uma_imagem])  for  uma_imagem in IMAGEM
+        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGEM
         for ordem, carta in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
                 carta.direita = self.cartas[ordem+1]
     def baralho(self):
         return self.cartas
         
-class Jogo:
+class Jogo:    
     def __init__(self):
         self.baralho = Carta().baralho()
         self.templo = Cena(DI["CENA"])

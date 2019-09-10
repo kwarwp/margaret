592a9bc0f7a24f86434ec83c80a3d7a7670cd89a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:55:37 -0300

    Saved kellee/__log__.py at Timestamp: 2019-09-10 13:55:36

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -382,3 +382,10 @@ SyntaxError: invalid syntax
                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 10 2019 10:55:27.403 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 28
+  def set_esquerda(self, esquerda)
+                                   ^
+SyntaxError: invalid syntax
+'''},

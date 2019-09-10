b0b0cef3da6f900421ce08d5a78c1046d4c7c396
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:28:46 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-10 13:28:45

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -350,3 +350,10 @@ SyntaxError: invalid syntax
                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 10 2019 10:28:50.477 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 46
+  self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
+                                                                                                                                              ^
+SyntaxError: invalid syntax
+'''},

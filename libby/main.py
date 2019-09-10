c3232b8d8aa566ca644f714621613869ffb10860
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:51:59 -0300

    Saved libby/main.py at Timestamp: 2019-09-10 13:51:59

--- libby/main.py
+++ libby/main.py
@@ -20,29 +20,43 @@ DIC["ARANHA"] = "https://i.imgur.com/tQLsXB0.jpg"
 DIC["MUMIA"] = "https://i.imgur.com/OVSH3Aa.jpg"
 DIC["FOGO"] = "https://i.imgur.com/LmoJDRb.jpg"
 DIC["DESMORONAMENTO"] = "https://i.imgur.com/Wl29PRv.jpg"
+ACAMPAMENTO = Cena("https://i.imgur.com/LYVTX9p.jpg")
 
 class Perigo: 
     def __init__(self, tipo, imagem):
         self.cena = Cena(imagem)
+        self.tipo = tipo
         self.cena_vai = self.cena.vai
         self.cena.vai = self.vai
+        self.acampamento = ACAMPAMENTO
+        
+    def set_direita (self, direita):
+        self.cena.direta = direita
+        
+    def set_direita (self, direita):
+        self.cena.direta = direita
+        
     def vai(self):
         if self.tipo in PERIGOS:
+            self.cena.direita = self.acampamento 
         else:
-             PERIGOS[self.tipo] = 1
+             PERIGOS[self.tipo]*1
         self.cena_vai()   
         
 class Cartas: 
     def __init__(self):
-        self.cartas = [Cena(DIC[uma_imagem]) for uma_imagem in IMAGENS]
+        self.cartas = [Perigo(DIC[uma_imagem], uma_imagem) 
+            for uma_imagem in IMAGENS]
         for ordem, cartas in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
-               cartas.direita = self.cartas [ordem+1]
+               cartas.set_direita = (self.cartas [ordem+1])
     def baralho(self):
         return self.cartas
         
 class Jogo:
     def __init__(self):
+        global PERIGOS
+        PERIGOS = {}
         self.baralho = Cartas().baralho()
         self.templo = Cena(DIC["TEMPLO"])
         self.templo.direita = self.baralho[1]

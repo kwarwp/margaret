9ab6dce3d419a7fb2d4e6adc3e4be410b4486851
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:34:45 -0300

    Saved ruzwana/__log__.py at Timestamp: 2019-09-10 13:34:44

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -182,3 +182,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Sep 10 2019 10:34:45.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  self.cena.direita = self.acampamento
+  ^
+IndentationError: expected an indented block
+'''},

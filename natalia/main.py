c32c23922fab86f188df12acd3bc6b20ab98470a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:50:06 -0300

    Saved natalia/main.py at Timestamp: 2019-09-10 13:50:06

--- natalia/main.py
+++ natalia/main.py
@@ -10,24 +10,7 @@ DI["PEDRAS"] = "https://i.imgur.com/4ftVqRs.jpg"
 DI["ARTEFATO"] = "https://i.imgur.com/rGbY5XC.jpg"
 
 
-class Carta:
-    def __init__(self):
-        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
-        for ordem, carta in enumerate(self.cartas):
-            if ordem < len(self.cartas)-1:
-                carta.direita = self.cartas[ordem+1]
-    def baralho(self):
-        return self.cartas
-        
-class Jogador:
-    def __init__(self):
-        self.chance = list(range(30))
-        shuffle(self.chance)
-        self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
-        self.maior_tesouro, self.maior_joias, = [0]*6
-        self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
-        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
-        
+ACAMPAMENTO = Cena("https://i.imgur.com/dmSDeDF.jpg")      
 class Perigo:
     def __init__(self, imagem, tipo):
         self.cena = Cena(imagem)
@@ -51,3 +34,20 @@ class Perigo:
             PERIGOS[self.tipo] = 1
         self.cena_vai()
         
+class Carta:
+    def __init__(self):
+        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
+        for ordem, carta in enumerate(self.cartas):
+            if ordem < len(self.cartas)-1:
+                carta.direita = self.cartas[ordem+1]
+    def baralho(self):
+        return self.cartas
+        
+class Jogo:
+    def __init__(self):
+        self.baralho = Carta().baralho()
+        #self.templo = Cena(DI["TEMPLO"])
+        self.templo = Perigo(tipo="TEMPLO", imagem=DI["TEMPLO"])
+        #self.templo.direita = self.baralho[1]
+    def inicia(self):
+        self.templo.vai()

2512a7de82d615f6ed6383d7e6b44e34c1bec73d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:52:58 -0300

    Saved natalia/main.py at Timestamp: 2019-09-10 13:52:58

--- natalia/main.py
+++ natalia/main.py
@@ -1,15 +1,17 @@
 # margaret.natalia.main.py
+__author__ = "gabrielly de souza silveira dos reis bielly2reis@gmail.com "
 from _spy.vitollino.main import Cena
-from parisa.main import SEREIAMONSTRO
-IMAGENS = ["CENA", "PEDRAS", "TEMPLO", "ARTEFATO", "SEREIAMONSTRO"]*5
+from ruzwana.main import DI as RDI
+from random import shuffle
+#from stacy.main import fogo
+#from natalia.main import sereiamonstro
+IMAGENS = ["FOGO", "PEDRAS", "ARANHA", "MUMIA", "DESMORONAMENTO"]*5
 shuffle(IMAGENS)
+PERIGOS = {}
+
 DI = DICIONARIO_DE_IMAGENS = {}
-DI["TEMPLO"] = "https://i.imgur.com/0EsXIPN.jpg"
+DI["TEMPLO"] = "https://i.imgur.com/7GZetDn.jpg"
 DI["TESOURO"] = "https://i.imgur.com/h8MfuRD.jpg"
-DI["PEDRAS"] = "https://i.imgur.com/4ftVqRs.jpg"
-DI["ARTEFATO"] = "https://i.imgur.com/rGbY5XC.jpg"
-
-
 ACAMPAMENTO = Cena("https://i.imgur.com/dmSDeDF.jpg")      
 class Perigo:
     def __init__(self, imagem, tipo):
@@ -36,18 +38,27 @@ class Perigo:
         
 class Carta:
     def __init__(self):
-        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
+        self.cartas = [Perigo(RDI[uma_imagem], uma_imagem) 
+            for uma_imagem in IMAGENS]
         for ordem, carta in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
-                carta.direita = self.cartas[ordem+1]
+                carta.set_direita(self.cartas[ordem+1])
     def baralho(self):
         return self.cartas
         
 class Jogo:
     def __init__(self):
+        global PERIGOS
+        PERIGOS ={}
         self.baralho = Carta().baralho()
-        #self.templo = Cena(DI["TEMPLO"])
-        self.templo = Perigo(tipo="TEMPLO", imagem=DI["TEMPLO"])
-        #self.templo.direita = self.baralho[1]
+        self.templo = Cena(DI["TEMPLO"])
+        #self.templo = Perigo(tipo="TEMPLO", imagem=DI["TEMPLO"])
+        self.templo.direita = self.baralho[1]
     def inicia(self):
         self.templo.vai()
+        
+
+inca = Jogo()  # Inca()
+
+if __name__ == "__main__":
+    inca.inicia()

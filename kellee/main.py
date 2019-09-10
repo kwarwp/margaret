3eb4c7b94eb630de31814213a062518856cfb86e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:37:06 -0300

    Saved kellee/main.py at Timestamp: 2019-09-10 13:37:06

--- kellee/main.py
+++ kellee/main.py
@@ -4,6 +4,8 @@ from _spy.vitollino.main import Cena
 from random import shuffle
 IMAGENS = ["CENA", "TESOURO", "MONSTRO", "FOGO", "PEDRAS", "MUMIA"]*8
 shuffle(IMAGENS)
+PERIGOS = {}
+
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["CENA"] = "https://i.imgur.com/F0pWLRM.png"
 DI["TESOURO"] = "https://i.imgur.com/Z2CD83L.jpg"
@@ -14,7 +16,7 @@ DI["MUMIA"] = "https://i.imgur.com/4xWmf5O.jpg"
 DI["PEPITA DE OURO"] = "https://i.imgur.com/wtTTD26.png"
 DI["PEPITA OBSIDIANA"] = "https://i.imgur.com/DAtKJFX.png"
 DI["PEPITA TURQUESA"] = "= https://i.imgur.com/fnpiuCo.jpg" 
- 
+ACAMPAMENTO = CENA ("https://i.imgur.com/59m6gMz.jpg")
 class Perigo:
     def __init__(self, imagem, tipo):
        self.cena = Cena(imagem)
@@ -30,11 +32,14 @@ class Perigo:
         self.cena.esquerda = direita 
      
     def vai(self):
+        if self.tipo in PERIGOS:
+        # deu ruim, já tinha aparecido um perigo
         self.cena_vai()
         
 class Carta:
     def __init__(self):
-        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS] 
+        self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
+        for uma_imagem in IMAGENS] 
         for ordem, carta in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
                 carta.direita = self.cartas[ordem+1]

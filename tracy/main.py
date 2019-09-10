b858a5ab0d45cd89db6ac56fb3fd5a8b8afee71a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:20:14 -0300

    Saved tracy/main.py at Timestamp: 2019-09-10 13:20:13

--- tracy/main.py
+++ tracy/main.py
@@ -1,47 +1,4 @@
 # margaret.tracy.main.py
 __author__ = "gabrielly de souza silveira dos reis bielly2reis@gmail.com "
-from _spy.vitollino.main import Cena
-from ruzwana.main import DI as RDI
-from random import shuffle
-#from stacy.main import fogo
-#from natalia.main import sereiamonstro
-IMAGENS = ["FOGO", "PEDRAS", "ARANHA", "MUMIA", "DESMORONAMENTO"]*5
-shuffle(IMAGENS)
-DI = DICIONARIO_DE_IMAGENS = {}
-DI["TEMPLO"] = "https://i.imgur.com/7GZetDn.jpg"
-DI["TESOURO"] = "https://i.imgur.com/h8MfuRD.jpg"
-        
-class Perigo:
-    def __init__(self, imagem, tipo):
-        self.cena = Cena(imagem)
-        self.tipo = tipo
-        self.cena_vai = self.cena.vai
-        self.cena.vai = self.vai
-        
-    def vai(self):
-        self.cena_vai()
-        
-class Carta:
-    def __init__(self):
-        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
-        for ordem, carta in enumerate(self.cartas):
-            if ordem < len(self.cartas)-1:
-                carta.direita = self.cartas[ordem+1]
-    def baralho(self):
-        return self.cartas
-        
-class Jogo:
-    def __init__(self):
-        self.baralho = Carta().baralho()
-        #self.templo = Cena(DI["TEMPLO"])
-        self.templo = Perigo(tipo="TEMPLO", imagem=DI["TEMPLO"])
-        #self.templo.direita = self.baralho[1]
-    def inicia(self):
-        self.templo.vai()
-        
 
-inca = Jogo()  # Inca()
-
-if __name__ == "__main__":
-    inca.inicia()
     

c84ebf8d50f3e2f3e7cf7aaef0d4add92bb86ff5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:58:59 -0300

    Saved natalia/__log__.py at Timestamp: 2019-09-10 13:58:59

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -1268,3 +1268,23 @@ NameError: name 'shuffle' is not defined
     shuffle(IMAGENS)
 NameError: name 'shuffle' is not defined
 '''},
+{'date': 'Tue Sep 10 2019 10:58:59.950 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    inca = Jogo()  # Inca()
+  module <module> line 56
+    self.baralho = Carta().baralho()
+  module <module> line 44
+    self.cartas = [Perigo(RDI[uma_imagem], uma_imagem) 
+KeyError: TESOURO
+'''},

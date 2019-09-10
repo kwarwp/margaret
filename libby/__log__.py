1314c72216abc74d56cea8d0999d450f14ae946b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 08:24:16 -0300

    Saved libby/__log__.py at Timestamp: 2019-09-10 11:24:16

--- libby/__log__.py
+++ libby/__log__.py
@@ -165,3 +165,23 @@ NameError: name 'Carta' is not defined
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 10 2019 08:24:18.151 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    inca = Jogo() 
+  module <module> line 34
+    self.baralho = Cartas().baralho()
+  module <module> line 28
+    carta.direita = self.cartas [ordem+1]
+NameError: name 'carta' is not defined
+'''},

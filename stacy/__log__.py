92830d282dd8719a8f9eaa1fbbe5bb165094d0ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 11:04:13 -0300

    Saved stacy/__log__.py at Timestamp: 2019-09-10 14:04:11

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1453,3 +1453,23 @@ IndentationError: unexpected indent
     self.cartas = [Perigo(DI[uma_imagem], uma_imagem) for uma_imagem in IMAGENS]
 TypeError: object() takes no parameters
 '''},
+{'date': 'Tue Sep 10 2019 11:04:10.473 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    inca = Jogo()
+  module <module> line 97
+    self.baralho = Cartas().baralho()
+  module <module> line 86
+    self.cartas = [Perigo(DI[uma_imagem], uma_imagem) for uma_imagem in IMAGENS]
+TypeError: object() takes no parameters
+'''},

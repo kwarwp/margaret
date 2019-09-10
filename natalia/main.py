4f736d0068bdfaebcfba7fffda4b21eb86f0fb61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 09:13:16 -0300

    Saved natalia/main.py at Timestamp: 2019-09-10 12:13:15

--- natalia/main.py
+++ natalia/main.py
@@ -1,6 +1,31 @@
 # margaret.natalia.main.py
 from _spy.vitollino.main import Cena
 from parisa.main import SEREIAMONSTRO
+IMAGENS = ["CENA", "PEDRAS", "TEMPLO", "ARTEFATO", "SEREIAMONSTRO"]*5
+shuffle(IMAGENS)
+DI = DICIONARIO_DE_IMAGENS = {}
+DI["TEMPLO"] = "https://i.imgur.com/7GZetDn.jpg"
+DI["TESOURO"] = "https://i.imgur.com/h8MfuRD.jpg"
+
+class Carta:
+    def __init__(self):
+        self.cartas = [Cena(RDI[uma_imagem]) for uma_imagem in IMAGENS]
+        for ordem, carta in enumerate(self.cartas):
+            if ordem < len(self.cartas)-1:
+                carta.direita = self.cartas[ordem+1]
+    def baralho(self):
+        return self.cartas
+        
+class Jogador:
+    def __init__(self):
+        self.chance = list(range(30))
+        shuffle(self.chance)
+        self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
+        self.maior_tesouro, self.maior_joias, = [0]*6
+        self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
+        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
+        
+        
 CENA = "http://i.imgur.com/loO50ff.jpg"
 ARTEFATO = "https://i.imgur.com/rGbY5XC.jpg"
 PEDRAS = "https://i.imgur.com/4ftVqRs.jpg"
@@ -20,3 +45,14 @@ pedras.vai()
 sereiamonstro.direita = fogo 
 fogo.esquerda = pedras
 pedras.vai()
+class Jogador:
+    def __init__(self):
+        self.chance = list(range(30))
+        shuffle(self.chance)
+        self.perigos, self.artefatos, self.cartas, self.rodada_corrente,\
+        self.maior_tesouro, self.maior_joias, = [0]*6
+        self.jogadores_jogando, self.tesouros_na_tenda, self.cartas_na_mesa,\
+        self.tesouros_na_mesa, self.tesouros_jogadores, self.joias_jogadores = [[]]*6
+        
+       class Jogo: 
+       

b101002d4dd9ff48f9382c3bb4b8dcdc4e0c0d76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 10:39:30 -0300

    Saved kellee/main.py at Timestamp: 2019-09-10 13:39:29

--- kellee/main.py
+++ kellee/main.py
@@ -4,8 +4,6 @@ from _spy.vitollino.main import Cena
 from random import shuffle
 IMAGENS = ["CENA", "TESOURO", "MONSTRO", "FOGO", "PEDRAS", "MUMIA"]*8
 shuffle(IMAGENS)
-PERIGOS = {}
-
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["CENA"] = "https://i.imgur.com/F0pWLRM.png"
 DI["TESOURO"] = "https://i.imgur.com/Z2CD83L.jpg"
@@ -16,30 +14,20 @@ DI["MUMIA"] = "https://i.imgur.com/4xWmf5O.jpg"
 DI["PEPITA DE OURO"] = "https://i.imgur.com/wtTTD26.png"
 DI["PEPITA OBSIDIANA"] = "https://i.imgur.com/DAtKJFX.png"
 DI["PEPITA TURQUESA"] = "= https://i.imgur.com/fnpiuCo.jpg" 
-ACAMPAMENTO = CENA ("https://i.imgur.com/59m6gMz.jpg")
+ 
 class Perigo:
     def __init__(self, imagem, tipo):
        self.cena = Cena(imagem)
        self.tipo = tipo
        self.cena_vai = self.cena.vai
        self.cena.vai = self.vai
-       self.acampamento = ACAMPAMENTO
-    
-    def set_direita(self, direita):
-        self.cena.direita = direita
-        
-    def set_esquerda(self, esquerda)
-        self.cena.esquerda = direita 
      
     def vai(self):
-        if self.tipo in PERIGOS:
-        # deu ruim, já tinha aparecido um perigo
         self.cena_vai()
         
 class Carta:
     def __init__(self):
-        self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
-        for uma_imagem in IMAGENS] 
+        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS] 
         for ordem, carta in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
                 carta.direita = self.cartas[ordem+1]

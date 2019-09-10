ab895cd1aecd0647367715729ed7052373114423
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 09:32:37 -0300

    Saved ruzwana/main.py at Timestamp: 2019-09-10 12:32:37

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -16,30 +16,7 @@ DI["ARTEFATO"] = "https://i.imgur.com/vN9MDwx.jpg"
 DI["FOGO"] = "https://i.imgur.com/Mbek5ie.jpg"
 DI["ARANHA"] = "https://i.imgur.com/k2RoQqf.jpg"
 DI["COBRA"] = "https://i.imgur.com/k2RoQqf.jpg"
-    
-class Carta:
-    def __init__(self):
-        self.cartas = [Cena(DI[uma_imagem]) for uma_imagem in IMAGENS]
-        for ordem, carta in enumerate(self.cartas):
-            if ordem < len(self.cartas)-1:
-                carta.direita = self.cartas[ordem+1]
         
-    def baralho(self):
-        return self.cartas
-
-class Jogo:
-    def __init__(self):
-        self.baralho = Carta().baralho()
-        self.templo = Cena(DI["TEMPLO"])
-        self.templo.direita  = self.baralho[0]
-    def inicia(self):
-        self.templo.vai()
-
-inca = Jogo()
-
-if __name__ == "__main__":
-    inca.inicia()
-    
 class Perigo:
     def _init__(self, imagem, tipo):
         self.cena = Cena(imagem)
@@ -66,3 +43,8 @@ class Jogo:
         self.templo.direita = self.baralho[1]        
     def inicia(self):
         self.templo.vai()
+        
+inca = Jogo()
+
+if __name__ == "__main__":
+    inca.inicia()

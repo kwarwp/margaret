a9a8a46ffda197fe377991ae02f49225764e3793
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-10 11:01:08 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-10 14:01:08

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -498,3 +498,23 @@ NameError: name 'Carta' is not defined
     self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
 KeyError: ACAMPAMENTO
 '''},
+{'date': 'Tue Sep 10 2019 11:01:12.575 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    inca = Jogo()  # Inca()
+  module <module> line 59
+    self.baralho = Carta().baralho()
+  module <module> line 47
+    self.cartas = [Perigo(DI[uma_imagem], uma_imagem) 
+KeyError: ACAMPAMENTO
+'''},

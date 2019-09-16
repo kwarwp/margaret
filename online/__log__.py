a68ca8a2bb19ce0300dab8dfc25f9712f78eb1c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 12:45:02 -0300

    Saved online/__log__.py at Timestamp: 2019-09-16 15:45:02

--- online/__log__.py
+++ online/__log__.py
@@ -63,3 +63,34 @@ SyntaxError: invalid syntax
 'error': '''
 Exception: <SyntaxError: expected expression, got keyword 'var'>
 '''},
+{'date': 'Mon Sep 16 2019 12:45:01.43 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from types import SimpleNameSpace as Sns
+ImportError: cannot import name 'SimpleNameSpace'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from types import SimpleNameSpace as Sns
+'''},

8305553323382a5282b65ffc485a158936960625
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 20:23:29 -0300

    Created __author__.py @2019-09-17 20:23

---
+++ __author__.py
@@ -0,0 +1,2 @@
+{
+"margaret": {"author_nick": "fonocomp1_19", "author_name": "FonoComp I 2019", "author_email": "carlo@nce.ufrj.br", "author_org": "UFRJ", "author_site": "http://labase.nce.ufrj.br", "author_public": "on"},

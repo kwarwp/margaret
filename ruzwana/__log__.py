4c330d853f2c74234498f66f871826fc5cdde608
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 09:17:17 -0300

    Saved ruzwana/__log__.py at Timestamp: 2019-09-17 12:17:17

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -310,3 +310,21 @@ KeyError: CARTAS
     self.baralho = Carta().baralho()
 NameError: name 'Carta' is not defined
 '''},
+{'date': 'Tue Sep 17 2019 09:17:18.181 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    inca = Jogo()
+  module <module> line 60
+    self.baralho = CartaS().baralho()
+NameError: name 'CartaS' is not defined
+'''},

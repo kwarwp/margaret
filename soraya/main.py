26fa9178155d60f8b878284baaf0ae604f1085ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 09:58:21 -0300

    Saved soraya/main.py at Timestamp: 2019-09-17 12:58:21

--- soraya/main.py
+++ soraya/main.py
@@ -1,8 +1,16 @@
 # margaret.soraya.main.py
 __author__ "Vitória da Costa Lima vitcardinnot@gmail.com
 from spy.vitollino.main import Cena
-CENA = ""
-TESOURO = ""
+DI = DICIONARIO_DE_IMAGENS = {} 
+"CENA" = ""
+"TESOURO" = "https://i.imgur.com/tI5hg9u.jpg"
+TEMPLO = "https://i.imgur.com/jnxWklS.jpg"
+COBRA = "
+DESABAMENTO =
+MUMIA =
+ARANHA =
+FOGO =
+
 templo = Cena(CENA)
 tesouro = Cena(TESOURO)
 templo.direta = tesouro

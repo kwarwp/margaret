ad56378ff7f94a52e9246f43cdbe0b526472d90a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 22:36:31 -0300

    Saved stacy/main.py at Timestamp: 2019-09-17 01:36:31

--- stacy/main.py
+++ stacy/main.py
@@ -10,17 +10,17 @@ PERIGOS = {}
 
 DI = DICIONARIO_DE_IMAGENS = {}
 DI["TEMPLO"] = "https://i.imgur.com/LXptu0U.jpg"
-DI["TESOURO"] = "https://i.imgur.com/Nq1hCeU.jpg"
+#DI["TESOURO"] = "https://i.imgur.com/Nq1hCeU.jpg"
 DI["FOGO"] = "https://i.imgur.com/KRK66bR.jpg"
-DI["ARTEFATO1"] = "https://i.imgur.com/1QHNdyI.jpg"
+#DI["ARTEFATO1"] = "https://i.imgur.com/1QHNdyI.jpg"
 DI["COBRA"] = "https://i.imgur.com/MydpgBT.jpg"
 DI["DESABAMENTO"] = "https://i.imgur.com/jnxWklS.jpg"
 DI["MUMIA"] = "https://i.imgur.com/HPp1k5T.jpg" 
 DI["ARANHA"] = "https://i.imgur.com/w90m1jf.jpg"
-DI["SALADOTESOURO"] = "https://i.imgur.com/83xewyg.jpg"
-DI["PEPITADEOURO"] = "https://i.imgur.com/tsP6aby.jpg"
-DI["OBSIDIANA"] = "https://i.imgur.com/1Pqs1JN.jpg"
-DI["TURQUESA"] = "https://i.imgur.com/yIhLHaK.jpg"
+#DI["SALADOTESOURO"] = "https://i.imgur.com/83xewyg.jpg"
+#DI["PEPITADEOURO"] = "https://i.imgur.com/tsP6aby.jpg"
+#DI["OBSIDIANA"] = "https://i.imgur.com/1Pqs1JN.jpg"
+#DI["TURQUESA"] = "https://i.imgur.com/yIhLHaK.jpg"
 ACAMPAMENTO = Cena("https://i.imgur.com/yIhLHaK.jpg")
 
 class Inca:

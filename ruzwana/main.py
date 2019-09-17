816054471ac58577c4a0de49cea786d5954e201f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 10:33:48 -0300

    Saved ruzwana/main.py at Timestamp: 2019-09-17 13:33:48

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -44,8 +44,7 @@ class Perigo:
 
 class Cartas:
     def __init__(self):
-        self.cartas = [Perigo(DI[uma_imagem], uma_imagem)
-            for uma_imagem in IMAGENS]
+        self.cartas = [Perigo(DI[uma_imagem], uma_imagem) for uma_imagem in IMAGENS]
         for ordem, cartas in enumerate(self.cartas):
             if ordem < len(self.cartas)-1:
                 cartas.set_direita = (self.cartas[ordem+1])

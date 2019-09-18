f5ad5c41cadee1d7aa80eb8ee9201bc368db1cd0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:08:09 -0300

    Saved alexa/main.py at Timestamp: 2019-09-18 20:08:09

--- alexa/main.py
+++ alexa/main.py
@@ -1,10 +1,33 @@
 # margaret.alexa.main.py
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/SN72LsK.jpg"
+TURQUESA = "https://i.imgur.com/nTK6oYD.png"
+ARANHA = "https://i.imgur.com/5nKC0ns.png"
 
+class Carta:    
+    def __init__(self):
+        self.carta=Cena(ARANHA).vai()
+    
+    def vai(self):
+        self.carta.vai()
+    
+    def anterior(self,anterior):
+        anterior.direita  =self.carta
+        self.carta.esquerda = anterior   
+        
 class Jogo: 
     def __init__(self):
-        Cena(TEMPLO).vai()
+        self.templo = Cena(TEMPLO)
+        self.carta = Carta()
+        self.carta.anterior(self.templo)
+        self.templo.vai()
         
 if __name__=="__main__":
     Jogo()
+    
+    
+    
+    
+    
+    
+    

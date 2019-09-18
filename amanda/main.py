5ca5ff8db6629c19ee7e02919bd215826cec28ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:41:24 -0300

    Saved amanda/main.py at Timestamp: 2019-09-18 20:41:24

--- amanda/main.py
+++ amanda/main.py
@@ -2,9 +2,37 @@
 #Código criado por Acacia Calegari
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/S6Ruv7c.jpg"
+TURQUESA = "https://i.imgur.com/jR1l6vN.png"
+ARANHA = "https://i.imgur.com/1Bz5EVM.png"
+ARTEFATO = "https://i.imgur.com/IgwQY1N.png"
+DESMORONAMENTO = "https://i.imgur.com/GYLGPyn.jpg"
+COBRA = "https://i.imgur.com/1S1LFUE.jpg"
 
+DESASTRES = [ARTEFATO, DESMORONAMENTO, COBRA, ARANHA]*5
+
+class Carta:
+    def __init__(self, imagem=ARANHA):
+        self.carta = Cena(imagem)
+           
+    def vai(self):
+        self.carta.vai()
+
+    def monta_baralho(self, anterior):
+        for imagem_carta in caos:
+        carta = Carta(imagem_carta)
+        carta.anterior(anterior.carta)
+        anterior = carta
+        
+    def anterior(self, anterior)
+        anterior.direita = self.carta
+        self.carta.esquerda = anterior
+        
 class Jogo:
-	def __init__(self):
-    	Cena(TEMPLO).vai()
+    def __init__(self):
+        self.templo = Cena(TEMPLO)
+        self.carta = Carta()
+        self.carta.anterior(self.templo)
+        self.templo.vai()
+        
 if __name__ == "__main__":
 	Jogo()

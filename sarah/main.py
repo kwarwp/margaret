6a37d5e4652995655a1e14580f3089609cddd3b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:29:42 -0300

    Saved sarah/main.py at Timestamp: 2019-09-18 20:29:42

--- sarah/main.py
+++ sarah/main.py
@@ -1,12 +1,34 @@
 # margaret.julia.main.py
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/eiRyeJf.jpg"
+
+""" PEDRAS """
 TURQUESA = "https://i.imgur.com/jcJOmVD.png"
+
+""" ARTEFATOS """
+ARTa = "https://i.imgur.com/qyzLwer.png"
+
+""" CAOS """
 ARANHA = "https://i.imgur.com/lt6nVqj.gif"
+DESMORONAMENTO = "https://i.imgur.com/SzTXOyb.png"
+COBRA = "https://i.imgur.com/ZGHOKIz.png"
+LAVA = "https://i.imgur.com/TadcMw6.jpg"
+MUMIA = "https://i.imgur.com/krrhvL6.jpg"
+
+
+""" Listas """
+caos = [ARANHA, DESMORONAMENTO, COBRA, LAVA, MUMIA]*5
+pedras = [TURQUESA]
+artefatos = [ARTa]
 
 class Carta:
-	def __init__(self):
-		self.carta = Cena(ARANHA).vai()  
+	def __init__(self, cena=ARANHA):
+		self.carta = Cena(cena)
+		anterior = self
+		for carta in caos:
+			umacarta = Carta(carta)
+			umacarta.anterior(anterior.carta)
+			anterior = umacarta
         
 	def vai(self):
      	 self.carta.vai()
@@ -20,7 +42,7 @@ class Jogo:
 	def __init__(self):
 		self.templo = Cena(TEMPLO)
 		self.carta = Carta()
-		self.carta.proximo(self.templo)
+		self.carta.anterior(self.templo)
 		self.templo.vai()
         
         

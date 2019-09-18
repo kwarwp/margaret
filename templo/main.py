2cb2616cd4d4df131789d7a88c76d0304e656717
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:08:48 -0300

    Saved templo/main.py at Timestamp: 2019-09-18 20:08:48

--- templo/main.py
+++ templo/main.py
@@ -1,11 +1,26 @@
 # margaret.templo.main.py
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/IKMbeR2.jpg"
+TURQUESA = "https://i.imgur.com/tS8YRxw.jpg"
+ARANHA = "https://i.imgur.com/0BUoL0u.jpg"
 
-
+class Carta:
+    def __init__(self):
+        self.carta = Cena(ARANHA)
+        
+    def vai(self):
+        self.carta.vai()
+        
+    def anterior(self, anterior):
+        anterior.direita = self.carta
+        self.carta.esquerda = anterior
+        
 class Jogo:
     def __init__(self):
-        Cena(TEMPLO).vai()
+        self.templo = Cena(TEMPLO)
+        self.carta = Carta()
+        self.carta.anterior(self.templo)
+        self.templo.vai()
         
 if __name__ == "__main__":
     Jogo()

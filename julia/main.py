b3f97fe72d24d34fd9a8bd7675cf301797776bcd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 16:52:33 -0300

    Saved julia/main.py at Timestamp: 2019-09-18 19:52:33

--- julia/main.py
+++ julia/main.py
@@ -1,4 +1,10 @@
 # margaret.julia.main.py
 from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/eiRyeJf.jpg"
-Cena(TEMPLAO).vai()
+
+class Jogo:
+	def _init__(self):
+		Cena(TEMPLO).vai()
+        
+if __name__ == "__main__":
+	Jogo()

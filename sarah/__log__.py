ea00cca000a091d12dfdff10d48cc456b6511a49
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:06:20 -0300

    Saved sarah/__log__.py at Timestamp: 2019-09-18 20:06:19

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -174,3 +174,21 @@ AttributeError: 'Carta' object has no attribute 'proximo'
     		self.carta.proximo(self.templo)
 AttributeError: 'Carta' object has no attribute 'proximo'
 '''},
+{'date': 'Wed Sep 18 2019 17:06:23.162 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    	Jogo()
+  module <module> line 23
+    		self.carta.proximo(self.templo)
+AttributeError: 'Carta' object has no attribute 'proximo'
+'''},

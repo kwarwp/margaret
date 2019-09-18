fa9c3f24d8c6043f281977f146c71aba67df8caa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:29:39 -0300

    Saved alexa/main.py at Timestamp: 2019-09-18 20:29:39

--- alexa/main.py
+++ alexa/main.py
@@ -3,10 +3,21 @@ from _spy.vitollino.main import Cena
 TEMPLO = "https://i.imgur.com/SN72LsK.jpg"
 TURQUESA = "https://i.imgur.com/nTK6oYD.png"
 ARANHA = "https://i.imgur.com/5nKC0ns.png"
+ARTEFATO = "https://i.imgur.com/IITJkP0.png"
+DESMORONAMENTO = "https://i.imgur.com/HRBlc8Q.jpg"
+COBRA = "https://i.imgur.com/NEPd9FH.jpg"
+FOGO = "https://i.imgur.com/qK0szTH.jpg"
+
+DESASTRES = [ARTEFATO, DESMORONAMENTO, COBRA, ARANHA, FOGO]*5
 
 class Carta:    
-    def __init__(self):
-        self.carta=Cena(ARANHA).vai()
+    def __init__(self, cena=ARANHA):
+        self.carta = Cena(cena)
+        anterior = self
+        for carta in DESASTRE: 
+        umacarta = Carta(carta)
+        umacarta.anterior(anterior.carta)
+        anterior = umacarta
     
     def vai(self):
         self.carta.vai()

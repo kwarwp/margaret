81a6a01cdccfe42432d3bb945b2874ae0de5eb10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 16:53:50 -0300

    Created amanda/__log__.py at Timestamp: 2019-09-18 19:53:50

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 18 2019 16:53:49.281 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  Cena(TEMPLO).vai()
+  ^
+IndentationError: expected an indented block
+'''},

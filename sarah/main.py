699717c61722fb70a18d1a51f3f15a1acd96a7ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:06:22 -0300

    Saved sarah/main.py at Timestamp: 2019-09-18 20:06:21

--- sarah/main.py
+++ sarah/main.py
@@ -6,12 +6,22 @@ ARANHA = "https://i.imgur.com/lt6nVqj.gif"
 
 class Carta:
 	def __init__(self):
-		Cena(TEMPLO).vai()  
+		self.carta = Cena(ARANHA).vai()  
+        
+	def vai(self):
+     	 self.carta.vai()
+         
+	def anterior(self, anterior):
+		anterior.direita = self.carta
+		self.carta.esquerda = anterior
     
 
 class Jogo:
 	def __init__(self):
-		Cena(TEMPLO).vai()
+		self.templo = Cena(TEMPLO)
+		self.carta = Carta()
+		self.carta.proximo(self.templo)
+		self.templo.vai()
         
         
 if __name__ == "__main__":

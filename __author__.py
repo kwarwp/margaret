0cc30f0a4b3ecb4042f5bdda55ca45cbec386529
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 16:31:15 -0300

    add lisa

--- __author__.py
+++ __author__.py
@@ -1,2 +1,2 @@
 {
-"margaret": {"author_nick": "fonocomp1_19", "author_name": "FonoComp I 2019", "author_email": "carlo@nce.ufrj.br", "author_org": "UFRJ", "author_site": "http://labase.nce.ufrj.br", "author_public": "on"},
+"lisa": {"author_nick": "carlo_fonocomp1_19", "author_name": "FonoComp I 2019", "author_email": "carlo@nce.ufrj.br", "author_org": "UFRJ", "author_site": "http://labase.nce.ufrj.br", "author_public": "on"},

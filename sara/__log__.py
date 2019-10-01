5bd7b2716603140a42bcfde3bc4c58d7286da7fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:12:28 -0300

    Saved sara/__log__.py @2019-10-01 12:12

--- sara/__log__.py
+++ sara/__log__.py
@@ -64,3 +64,10 @@ SyntaxError: invalid syntax
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 01 2019 09:12:30.281 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  """ o que tem que ter no acampamento? """
+  ^
+IndentationError: expected an indented block
+'''},

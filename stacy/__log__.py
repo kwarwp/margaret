5d35844cd67b17b9fd3592d59b22720f8a85c92e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:21:48 -0300

    Saved stacy/__log__.py @2019-10-01 12:21

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1636,3 +1636,21 @@ SyntaxError: invalid syntax
     self.jogador = Jogador(self.acampamento)
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Tue Oct 01 2019 09:21:46.96 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 96
+    inca = Jogo()
+  module <module> line 87
+    self.jogador = Jogador(self.acampamento)
+TypeError: __init__() takes 1 positional argument but more were given
+'''},

79d53271ab31cd23cf52c0c9861a116f6c93f734
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:19:39 -0300

    Saved courtney/__log__.py @2019-10-01 12:19

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -286,3 +286,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 01 2019 09:19:40.77 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 40
+  def continua(self):   
+  ^
+IndentationError: expected an indented block
+'''},

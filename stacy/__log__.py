b18777da3b38f0e973af08278c5eee0fcbaf2776
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:27:47 -0300

    Saved stacy/__log__.py @2019-10-01 12:27

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1708,3 +1708,21 @@ NameError: name 'jogador' is not defined
     self.baralho = Carta(self.jogador).baralho()
 NameError: name 'Carta' is not defined
 '''},
+{'date': 'Tue Oct 01 2019 09:27:46.185 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    inca = Jogo()
+  module <module> line 90
+    self.baralho = Carta(self.jogador).baralho()
+NameError: name 'Carta' is not defined
+'''},

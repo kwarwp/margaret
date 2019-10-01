99b31d25fa15c750d9341b534c918a7b2359240b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 08:32:01 -0300

    Saved stacy/main.py @2019-10-01 11:32

--- stacy/main.py
+++ stacy/main.py
@@ -22,11 +22,6 @@ DI["PEPITADEOURO"] = "https://i.imgur.com/tsP6aby.jpg"
 DI["OBSIDIANA"] = "https://i.imgur.com/1Pqs1JN.jpg"
 DI["TURQUESA"] = "https://i.imgur.com/yIhLHaK.jpg"
 ACAMPAMENTO = Cena("https://i.imgur.com/wgcVh9M.jpg")
-
-class Acampamento:
-    def __init__(self):
-        pass
-        # o que tem que ter no acampamento?
     
 class Perigo:
     def __init__(self, imagem, tipo):

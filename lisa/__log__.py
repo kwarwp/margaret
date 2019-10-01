65f117e840ee15d23a0ec70068c13fcd38bd3249
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:43:49 -0300

    Saved lisa/__log__.py @2019-10-01 12:43

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -238,3 +238,23 @@ SyntaxError: invalid syntax
     nome = nome_item.tit or nome_item
 AttributeError: 'Elemento' object has no attribute 'tit'
 '''},
+{'date': 'Tue Oct 01 2019 09:43:49.778 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    inca = Jogo()  # Inca()
+  module <module> line 100
+    INVENTARIO.bota(tur)
+  module _spy.vitollino.main line 360
+    nome = nome_item.tit or nome_item
+AttributeError: 'Elemento' object has no attribute 'tit'
+'''},

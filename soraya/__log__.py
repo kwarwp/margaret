342901681dad42a15b7633d9f16fb1b3dc5c5a2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 10:50:56 -0300

    Created soraya/__log__.py @2019-10-01 13:50

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 01 2019 10:50:53.283 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  __author__ "Vitória da Costa Lima vitcardinnot@gmail.com
+                                                          ^
+SyntaxError: EOL while scanning string literal
+'''},

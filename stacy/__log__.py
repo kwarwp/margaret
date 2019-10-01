b076a7e24c56713f26104f55fe074935ca37d696
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:55:02 -0300

    Saved stacy/__log__.py @2019-10-01 12:55

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1800,3 +1800,21 @@ NameError: name 'TURQUESA' is not defined
     tur = Elemento(TURQUESA, tit="Turquesa", style=dict(left=5, top=50, width=25), cena=self.acampamento.cena)
 NameError: name 'TURQUESA' is not defined
 '''},
+{'date': 'Tue Oct 01 2019 09:55:01.194 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 103
+    inca = Jogo()
+  module <module> line 93
+    tur = Elemento(TURQUESA, tit="turquesa", style=dict(left=5, top=50, width=25), cena=self.acampamento.cena)
+NameError: name 'TURQUESA' is not defined
+'''},

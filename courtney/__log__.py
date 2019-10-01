d9565af5a6283efa837af031cfe64d6e451c579a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 09:20:42 -0300

    Saved courtney/__log__.py @2019-10-01 12:20

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -307,3 +307,10 @@ SyntaxError: invalid syntax
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 01 2019 09:20:42.305 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 46
+  pass Image
+  ^
+IndentationError: unexpected indent
+'''},

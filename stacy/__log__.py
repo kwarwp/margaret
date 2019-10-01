d8945000b4c7ef97797aea85f317478c965339fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 08:56:02 -0300

    Saved stacy/__log__.py @2019-10-01 11:56

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1575,3 +1575,25 @@ SyntaxError: invalid syntax
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 01 2019 08:56:00.760 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 91
+    inca = Jogo()
+  module <module> line 84
+    self.baralho = Cartas(self.jogador).baralho()
+  module <module> line 75
+    cartas.set_direita(self.cartas[ordem+1])
+  module <module> line 56
+    self.cena.direita = continua()
+NameError: name 'continua' is not defined
+'''},

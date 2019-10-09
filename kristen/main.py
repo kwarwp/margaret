bc0c835625e53904a3df4cd2ccb6204fa360e507
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-09 10:46:50 -0300

    Saved kristen/main.py @2019-10-09 13:46

--- kristen/main.py
+++ kristen/main.py
@@ -1,41 +1,100 @@
 # margaret.kristen.main.py
-from _spy.vitollino.main import Cena
-from random import shuffle
-TEMPLO = "https://i.imgur.com/hy9jUf5.jpg"
-TESOURO = "https://i.imgur.com/pcrB1gm.jpg"
-PERIGOS = {
-    "MUMIA": "https://i.imgur.com/vaRhKqK.jpg",
-    "COBRA": "https://i.imgur.com/oTs2Tf4.jpg",
-    "DESMORONAMENTO": "https://i.imgur.com/gNFtoMy.jpg",
-    "ARANHA": "https://i.imgur.com/gNFtoMy.jpg",
-    "FOGO": "https://i.imgur.com/4XA6XhB.jpg",
-} 
+# mary_shaw.roxanne.main.py
+# from adda.main import JogoTesouroInca as JogoMarilia
+"""
+    Tesouro Inca
+"""
+from _spy.vitollino.main import Cena, INVENTARIO
+from elemento.main import Elemento
+from random import choice
 
-class Cartas:
+__author__ = "Carlo"
+IMAGEM_DO_TEMPLO = "https://i.imgur.com/hYLSAKf.jpg"
+MUMIA = "https://i.imgur.com/YGV3CPB.jpg"
+COBRA = "https://i.imgur.com/IeC0gBj.jpg"
+TESOURO = "https://i.imgur.com/xkBdHCE.jpg"
+TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
+ACAMPAMENTO = "https://i.imgur.com/dmSDeDF.jpg"
+
+TUMBA = [COBRA, MUMIA] + [TESOURO]*3
+
+
+class Jogador():
+    """ Um explorador em busca de tesouros """
     def __init__(self):
-        self.baralho = [Cena(perigo) for perigo in PERIGOS.values()] * 5 
-        shuffle(self.baralho)
-        baralho_amanha = self.baralho[1:]
-        baralho_zip = zip(self.baralho, baralho_amanha)
-        for carta_hoje, carta_amanha in baralho_zip:
-            carta_hoje.direita = carta_amanha
-            carta_amanha.esquerda = carta_hoje
+        """ Inicia com tesouro """
+        self.tesouro = 0
+    
+    def ganho(self, valor):
+        """ Aumenta o tesouro com valor equivalente de turquesas 
+            :param valor: valor a acrescentar ao tesouro em número de Turquesas.        
+        """   
+        self.tesouro += valor
+        [INVENTARIO.bota("turquesa", TURQUESA) for _ in range(valor)]
 
-    def primeira_carta(self):
-        return self.baralho[0]
 
 
-class Jogo: 
-    def inicia(self):
-        templo = Cena (TEMPLO)
-        tesouro = Cena (TESOURO)
-        templo.direita = tesouro
-        tesouro.esquerda = templo 
-        perigos = Cartas()
-        umperigo = perigos.primeira_carta()
-        tesouro.direita = umperigo
-        umperigo.esquerda = tesouro 
-        templo.vai()
+class Tesouros(Cena):
+    """ Camaras secretas contendo tesouros """
+    def __init__(self, quantas_pedras=4, acampamento=None, eu=None):
+        """ Inicia a camara contendo umas pedras
+            :param int quantas_pedras: numero de pedras nesta camara
+        """
+        class ProximaCamara:
+            def vai(self):
+                pedras_na_camara = choice(range(1,5))
+                Tesouros(0, acampamento, eu).vai()
+                eu.ganho(pedras_na_camara)
+        self.tesouro = quantas_pedras
+        super().__init__(TESOURO, esquerda=acampamento, direita=ProximaCamara())
+        #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
+        self.pedras = [Elemento(
+             TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
+             range(self.tesouro)]
+
+
+class Tumba():
+    """ Um complexo de camaras secretas sob o templo """
+    def __init__(self, acampamento, eu):
+        """ Inicia o complexo de camaras """
+        self.tumba = [Tesouros(pedras+1, acampamento, eu) for pedras in range(4)]
+        self.inicial = choice(self.tumba)
+
+
+class Acampamento(Cena):
+    """ Um lugar seguro e quentinho para admirar seus ganhos """
+    def __init__(self, cena):
+        """ Cria a cena de um acampamento com tesouros """
+        self.tesouro = 4
+        super().__init__(cena)
+        #self.pedra = Elemento(TURQUESA, x=50, y=250, w=40, h=40, cena=self)
+        self.pedras = [Elemento(
+             TURQUESA, x=50+50*pedra, y=250, w=40, h=40, cena=self) for pedra in
+             range(self.tesouro)]
+
+    def ganho(self, valor):
+        """ aumenta o tesouro com um valor de pedras """
+        self.tesouro += valor
+
+
+class JogoTesouroInca:
+    """ Representa o Jogo principal """
+    def __init__(self):
+        """ Constroi a cena"""
+        INVENTARIO.inicia()
+        self.acampamento = Acampamento(ACAMPAMENTO)
+        self.eu = Jogador()
+        self.tumba = Tumba(self.acampamento, self.eu)
+        self.cena_do_templo = Cena(
+        IMAGEM_DO_TEMPLO, self.acampamento, direita=self.tumba.inicial)
+        self.acampamento.direita = self.cena_do_templo
         
-jogo = Jogo()
-jogo.inicia()
+    def inicia(self):
+        """ Inicia a construção do Jogo """
+        self.cena_do_templo.vai()
+        
+        
+if __name__ == "__main__":
+    jogo = JogoTesouroInca()
+    jogo.inicia()
+    #print(JogoTesouroInca,JogoTesouroInca())

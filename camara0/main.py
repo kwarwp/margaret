4e9167f2dab10682304ebf2019545251ac31c45b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:59:49 -0300

    Saved camara0/main.py @2019-10-15 12:59

--- camara0/main.py
+++ camara0/main.py
@@ -8,12 +8,12 @@ __author__ = "Junior"
 class camara:
     """ A camara é o lugar do jogo """
     def _init_(self):
-    """ as camaras são as divisões do templo """
-    self.camra = camara()  # importa a camara de outro módulo
-    self.explorador = Explorador()  # cria um explorador
+        """ as camaras são as divisões do templo """
+        self.camara = camara()  # importa a camara de outro módulo
+        self.explorador = Explorador()  # cria um explorador
     
     def inicia(self):
-    """ dá inicio ao jogo, fazendo o explorador entrar
+        """ dá inicio ao jogo, fazendo o explorador entrar
     na primmeira câmara
     """
-    self.camara.entrar(self.explorador)
+        self.camara.entrar(self.explorador)

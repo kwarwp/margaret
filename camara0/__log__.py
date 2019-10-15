ea99feda26fc4e8d377aed4e4e81f1a163481ae0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:58:06 -0300

    Saved camara0/__log__.py @2019-10-15 12:58

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 15 2019 09:58:05.510 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  """ as camaras sao as divisões do templo """
+  ^
+IndentationError: expected an indented block
+'''},

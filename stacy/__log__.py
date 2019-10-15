dd627628ecee1a5962e5d9c68688aaf9614bf26a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 08:59:30 -0300

    Saved stacy/__log__.py @2019-10-15 11:59

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -1924,3 +1924,10 @@ SyntaxError: invalid syntax
                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 15 2019 08:59:30.316 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 119
+  tesouros = [Tesouro(DI["PEDRAS"], pedras % 5, jogador) for pedras in range(3)] for pedras in range(30)]
+                                                                                  ^
+SyntaxError: invalid syntax
+'''},

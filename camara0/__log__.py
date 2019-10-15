5ccdf173688377563c31658aa1a705ad571fbe82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:58:14 -0300

    Saved camara0/__log__.py @2019-10-15 12:58

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -27,3 +27,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 15 2019 09:58:13.623 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  """ as camaras sao as divisoes do templo """
+  ^
+IndentationError: expected an indented block
+'''},

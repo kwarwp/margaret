54aad47bf577ff4a112f0609e2ab7f0cb5889051
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:56:49 -0300

    Created camara0/main.py @2019-10-15 12:56

---
+++ camara0/main.py
@@ -0,0 +1,19 @@
+# margaret.camara0.main.py
+"""
+Tesouro Inca - O Templo
+"""
+__author__ = "Junior"
+
+
+class camara:
+    """ A camara é o lugar do jogo """
+    def_init_(self):
+    """ as camaras são as divisões do templo """
+    self.camra = camara()  # importa a camara de outro módulo
+    self.explorador = Explorador()  # cria um explorador
+    
+    def inicia(self):
+    """ dá inicio ao jogo, fazendo o explorador entrar
+    na primmeira câmara
+    """
+    self.camara.entrar(self.explorador)

fcf588226ea2ec7159be1d0def6281bf15364458
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:42:13 -0300

    Saved professor10/main.py @2019-10-15 17:42

--- professor10/main.py
+++ professor10/main.py
@@ -1,7 +1,6 @@
 # margaret.professor10.main.py
 __author__ = "Alunos FonoComp 1_19"
 
-class Professor:
-    def __init__(self):
-        print("Feliz dia dos professores")
+class Professor_Carlo:
+    print("Feliz dia dos professores")
     

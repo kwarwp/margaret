bed23b37e71d274b712f622745ccc4ff1ad81bbd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:57:33 -0300

    Saved camara0/__log__.py @2019-10-15 12:57

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 15 2019 09:57:33.292 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  """ as camaras são as divisões do templo """
+  ^
+IndentationError: expected an indented block
+'''},

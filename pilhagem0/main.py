d791eacb081ca1b4b998478b619c0ace2c7b702f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:48:48 -0300

    Created pilhagem0/main.py @2019-10-15 12:48

---
+++ pilhagem0/main.py
@@ -0,0 +1,14 @@
+# margaret.pilhagem0.main.py
+"""
+Tesouro Inca - A pilhagem
+"""
+__author__ = "Victória Regina"
+
+class pilhagem:
+    """ A pilhagem é o lucro retirado do templo """
+    def __init__(self):
+        """ Cria a pilhagem com os seus valores """
+        self.pilhagem = pilhagem()
+        
+    def inicia(self):
+        

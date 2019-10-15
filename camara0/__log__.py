aa9e87a000d8c35a755956f38298731595d69d09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:58:39 -0300

    Saved camara0/__log__.py @2019-10-15 12:58

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -34,3 +34,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 15 2019 09:58:39.484 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  """ dá inicio ao jogo, fazendo o explorador entrar
+  ^
+IndentationError: expected an indented block
+'''},

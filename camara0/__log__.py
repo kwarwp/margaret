d5a30b940e6fdd6c3130ed2379a6e0853e2f4be6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:57:06 -0300

    Saved camara0/__log__.py @2019-10-15 12:57

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -6,3 +6,10 @@
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 15 2019 09:57:05.812 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  def_init_(self):
+                  ^
+SyntaxError: invalid syntax
+'''},

f0e1fc9d6ec7c9956cfdf9c645e3cf02773e8680
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 10:32:22 -0300

    Created cabana0/main.py @2019-10-15 13:32

---
+++ cabana0/main.py
@@ -0,0 +1,24 @@
+# margaret.cabana0.main.py
+"""
+Tesouro Inca = O Templo
+"""
+__author__ = "vitória"
+
+
+class Cabana:
+    """ a cabana guarda os tesouros """
+    def __init__ (self0:
+    """ cria a cabana com seus tesouros e também o explorador """
+    self.cabana = cabana() # importa a cabana de outro módulo
+    self.explorador = explorador() # cria um explorador
+    
+    def inicia(self):
+    """dá início aojogo, fazendo o jogador sair da cabana
+    
+    """
+    self.cabana.sai(self.explorador)
+    
+    if _name_ = "__main__"
+    
+    
+    

8abd6b4d34a5e7b06d76b145d8d8647627d4657e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 10:23:19 -0300

    Saved templo0/main.py @2019-10-15 13:23

--- templo0/main.py
+++ templo0/main.py
@@ -13,19 +13,22 @@ class Camara:
         explorador.pilhagem()
 
 class Explorador:
-    def __init__(self):
+    def __init__(self, camara):
+        self.camara = camara
         print("criou explorador")
         
     def pilhagem(self):
         """ saqueia a camara e fica com o tesouro"""
         print("saqueia a camara")
+        if input("continua?") == "s":
+            self.camara.entra(self)
 
 class Templo:
     """ O Templo Inca com camaras secretas """
     def __init__(self):
         """ Cria o templo com suas camaras e também o explorador """
         self.camara = Camara()  # importa a Camara de outro módulo
-        self.explorador = Explorador()  # cria um explorador
+        self.explorador = Explorador(self.camara)  # cria um explorador
         
     def inicia(self):
         """ dá início ao jogo, fazendo o explorador entrar

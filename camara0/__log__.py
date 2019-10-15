7c851729e3dc82697366f7ae34cf497f787d6ca8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:59:10 -0300

    Saved camara0/__log__.py @2019-10-15 12:59

--- camara0/__log__.py
+++ camara0/__log__.py
@@ -59,3 +59,21 @@ IndentationError: expected an indented block
     self.camra = camara()  # importa a camara de outro módulo
 NameError: name 'camara' is not defined
 '''},
+{'date': 'Tue Oct 15 2019 09:59:09.188 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    class camara:
+  module <module> line 12
+    self.camara = camara()  # importa a camara de outro módulo
+NameError: name 'camara' is not defined
+'''},

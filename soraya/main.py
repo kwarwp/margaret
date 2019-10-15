498b1487fc702d80ea2fa379784065bad038604b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:10:55 -0300

    Saved soraya/main.py @2019-10-15 12:10

--- soraya/main.py
+++ soraya/main.py
@@ -1,73 +1,162 @@
 # margaret.soraya.main.py
 __author__ = "Vitória da C. Lima vitcardinnot@gmail.com"
-from spy.vitollino.main import Cena, STYLE
-STYLE:["widht"] = 600
-STYLE:["height"] = 600
-from random import shuffle 
-IMAGENS = ["FOGO" , "COBRA" , "DESABAMENTO" , "MUMIA" , "ARANHA"]*5
-shuffle (IMAGENS)
+from _spy.vitollino.main import Cena, Elemento, INVENTARIO
+from ruzwana.main import DI as RDI
+from random import shuffle
+#from stacy.main import fogo
+#from natalia.main import sereiamonstro
+IMAGENS = ["FOGO", "COBRA", "ARANHA", "MUMIA",
+"DESMORONAMENTO"]*5
+#shuffle(IMAGENS)
 PERIGOS = {}
-
-DI = DICIONARIO_DE_IMAGENS = {} 
-DI["TEMPLO"] = "https://i.imgur.com/jnxWklS.jpg"
-DI["TESOURO"] = "https://i.imgur.com/tI5hg9u.jpg"
-DI["FOGO"] = "https://i.imgur.com/KRK66bR.jpg"
-DI["ARTEFATO1"] = "https://i.imgur.com/1QHNdyI.jpg"
-DI["COBRA"] = "https://i.imgur.com/MydpgBT.jpg"
-DI["DESABAMENTO"] = "https://i.imgur.com/jnxWklS.jpg"
-DI["MUMIA"] = "https://i.imgur.com/HPp1k5T.jpg" 
-DI["ARANHA"] = "https://i.imgur.com/w90m1jf.jpg"
-DI["SALADOTESOURO"] = "https://i.imgur.com/83xewyg.jpg"
-DI["PEPITADEOURO"] = "https://i.imgur.com/tsP6aby.jpg"
-DI["OBSIDIANA"] = "https://i.imgur.com/1Pqs1JN.jpg"
-DI["TURQUESA"] = "https://i.imgur.com/yIhLHaK.jpg"
-ACAMPAMENTO = Cena("https://i.imgur.com/wgcVh9M.jpg")
-class Perigo:
-    def __init__(self, imagem, tipo):
-        self.cena = Cena(imagem)
-        self.tipo = tipo
-        self.cena_vai = self.cena.vai
-        self.cena.vai = self.vai
-        self.acampamento = ACAMPAMENTO
-        
-    def set_direita(self, direita):
-        self.cena.direita = direita
-        
-    def set_esquerda(self, esquerda):
-        self.cena.esquerda = esquerda
-        
-    def vai(self):
-        if self.tipo in PERIGOS:
-            # deu ruim, já tinha aparecido um perigo
-            self.cena.direita = self.acampamento
-        else:
-            PERIGOS[self.tipo] = 1
-        self.cena_vai()
-
-
+DI = DICIONARIO_DE_IMAGENS = {}
+DI["TEMPLO"] = "https://i.imgur.com/7GZetDn.jpg"
+DI["TESOURO"] = "https://i.imgur.com/h8MfuRD.jpg"
+TURQUESA = "https://i.imgur.com/8WDBJM3.png" # DI["TURQUESA"]
 
 
 class Acampamento:
-     def __init__(self):
+    def __init__(self):
+        """ o que tem que ter no acampamento?"""
+        self.cena = Cena("https://i.imgur.com/dmSDeDF.jpg")
+        
+    def vai(self):
+        self.cena.vai()
+    
+
+
+class Jogador:
+    def __init__(self, acampamento):
+        """ o que tem que ter no jogador?
+            o jogador ganha uma turquesa para cada camara
+        """
+        self.turquesa = 0
+        self.acampamento = acampamento
+        # Aparato para capturar a ação clica na direita
+        self.cena_continua = Cena() # Chupa-cabras
+        self.cena_continua.vai = self.continua
+        # Aparato para capturar a ação clica na esquerda
+        self.cena_desiste = Cena()
+        self.cena_desiste.vai = self.desiste
+
+        
+    def continua(self):
+        """ entra em nova camara e acumula turquesas """
+        self.turquesa = self.turquesa + 1
         pass
-    # o que tem que ter no jogador
-    # o jogador ganha uma turquesa para cada camara
-     def continua(self):
-        pass
-      # acumula turquesa
-      
-     
-class Jogador
-      def __init__(self):
-      "" o que tem que ter no jogador
-         o jogador ganha uma turquesa para cada camara
-      """
-      self.cena_continua 
-         
-templo = Cena(CENA)
-tesouro = Cena(TESOURO)
-templo.direta = tesouro
-tesouro.esquerda = templo
-tesouro.direita = aranha
-aranha.esquerda = tesouro
-templo.vai()
+    def ganha_uma_turquesa(self, onde):
+        """
+        ouro = turquesa // 10
+        sobra_ouro = ouro % 10
+        obsidiana = sobra_ouro // 5
+        tur = sobra_ouro % 5
+        """
+        lugar =  50*onde
+        tur = Elemento(TURQUESA,  tit="Turquesa",
+            style=dict(left=f"{lugar}px", top="350px", width="50px",
+            height="30px"),
+                       cena=self.acampamento.cena)
+
+        
+    def desiste(self):
+        """ segue para o acampamento"""
+        self.coleta_tesouro()
+        self.acampamento.vai()
+        
+    def coleta_tesouro(self):
+        for tur in range(self.turquesa):
+            self.ganha_uma_turquesa(tur)
+        
+
+class Perigo:
+    def __init__(self, imagem, tipo, jogador):
+        self.jogador = jogador
+        self.cena = Cena(imagem)
+        self.tipo = tipo
+        # chupa cabra -- aparato
+        self.cena_vai = self.cena.vai
+        self.cena.vai = self.vai
+        self.acampamento = jogador.acampamento
+        self.set_esquerda(jogador.cena_desiste)
+
+    def set_direita(self, direita):
+        self.cena.direita = direita
+
+    def set_esquerda(self, esquerda):
+        self.cena.esquerda = esquerda
+    
+        
+    def vai(self):
+        if self.tipo in PERIGOS:
+            # deu ruim, já tinha aperecido um perigo
+            self.cena.direita = self.acampamento
+        else:
+            PERIGOS[self.tipo] = 1
+        #self.jogador.continua()
+        self.cena_vai()
+
+
+class Tesouro(Perigo):
+    def __init__(self, imagem, tipo, jogador):
+        super().__init__(imagem, tipo, jogador)
+        for tur in range(tipo+1):
+            self.ganha_uma_turquesa(tur)
+        
+    def vai(self):
+        self.jogador.continua()
+        self.cena_vai()
+        
+    def ganha_uma_turquesa(self, onde):
+        """
+        ouro = turquesa // 10
+        sobra_ouro = ouro % 10
+        obsidiana = sobra_ouro // 5
+        tur = sobra_ouro % 5
+        """
+        lugar =  50*onde
+        tur = Elemento(TURQUESA,  tit="Turquesa",
+            style=dict(left=f"{lugar}px", top="350px", width="50px",
+            height="30px"),
+                       cena=self.cena)
+        
+class Carta:
+    def __init__(self, jogador):
+        self.jogador = jogador
+        self.cartas = [Perigo(
+                              RDI[uma_imagem],
+                              uma_imagem,
+                              jogador) 
+            for uma_imagem in IMAGENS]
+        tesouros = [Tesouro(
+            RDI["PEDRAS"], pedras % 5, jogador)
+            for pedras in range(30)]
+        self.cartas += tesouros
+        shuffle(self.cartas)
+        shuffle(self.cartas)
+        for ordem, carta in enumerate(self.cartas):
+            if ordem < len(self.cartas)-1:
+                carta.set_direita(self.cartas[ordem+1])
+    def baralho(self):
+        return self.cartas
+        
+class Jogo:
+    def __init__(self):
+        global PERIGOS
+        PERIGOS ={}
+        INVENTARIO.inicia()
+        #INVENTARIO.bota(tur)
+        self.acampamento = Acampamento()
+        self.jogador = Jogador(self.acampamento)
+        self.baralho = Carta(self.jogador).baralho()
+        self.templo = Cena(DI["TEMPLO"])
+        #self.templo = Perigo(tipo="TEMPLO", imagem=DI["TEMPLO"])
+        self.templo.direita = self.baralho[1]
+    def inicia(self):
+        self.templo.vai()
+        
+
+inca = Jogo()  # Inca()
+
+if __name__ == "__main__":
+    inca.inicia()
+    

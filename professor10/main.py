dce30ebd4bff39a480cc24037ac1009978d2c7a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:32:45 -0300

    Created professor10/main.py @2019-10-15 17:32

---
+++ professor10/main.py
@@ -0,0 +1,10 @@
+# margaret.professor10.main.py
+__author__ = "Alunos FonoComp 1_19"
+
+class Professor:
+    def __init__(self):
+    self.Carlo = 10
+    print("Feliz dia dos professores")
+    
+if __name__ == "__main__":
+    professor,inicia()

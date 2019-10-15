6ae1fcbdb86abd4e44d792198cb2146bb85d2fa2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 09:56:55 -0300

    Created camara0/__log__.py @2019-10-15 12:56

---
+++ camara0/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 15 2019 09:56:52.671 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  def_init_(self):
+                  ^
+SyntaxError: invalid syntax
+'''},

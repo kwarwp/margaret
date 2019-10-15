0f1f32a514809c314ecb52bb41aa87a035d3f964
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-15 14:32:56 -0300

    Created professor10/__log__.py @2019-10-15 17:32

---
+++ professor10/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 15 2019 14:32:52.376 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  self.Carlo = 10
+  ^
+IndentationError: expected an indented block
+'''},

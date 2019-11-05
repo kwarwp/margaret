a427435101984f03a30fe567d27eefc0a491a4b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 09:46:54 -0300

    Saved danae/main.py @2019-11-05 12:46

--- danae/main.py
+++ danae/main.py
@@ -3,38 +3,41 @@
 Uma aventura de exploração
 """
 __author__ = "Carlo Oliveira <carlo at ufrj br>"
-__version__ = "19.11.05d"
+__version__ = "19.11.05e"
 
 class CamaraPerigosa:
-    """ Uma camara contendo um conteúdo misterioso. 
+    """ Uma camara contendo um perigos mortais. 
     O jogador entra nela quando se invoca o método vai
     """
-    def __init__(self):
+    def __init__(self, outra):
         self.camara = "Você entrou numa câmara com perigos."
         self.perigos = 0
+        self.outra = outra
         
     def vai(self):
         continua = " Segue para outra câmara? (s/N)"
         if input(self.camara+continua) == "s":
-            self.tesouros = self.tesouros + 1
-            return self.vai()
+            self.perigos = self.perigos + 1
+            return self.outra.vai()
         else:
             input("Você volta para a entrada do templo")
+            input(f"Você sai do templo mas encontrou {self.perigos} perigos")
             return self.tesouros
 
 class CamaraSecreta:
     """ Uma camara contendo um conteúdo misterioso. 
     O jogador entra nela quando se invoca o método vai
     """
-    def __init__(self):
+    def __init__(self, outra):
         self.camara = "Você entrou numa câmara com tesouros."
         self.tesouros = 0
+        self.outra = outra
         
     def vai(self):
         continua = " Segue para outra câmara? (s/N)"
         if input(self.camara+continua) == "s":
             self.tesouros = self.tesouros + 1
-            return self.vai()
+            return self.outra.vai()
         else:
             input("Você volta para a entrada do templo")
             input(f"Você sai do templo com {self.tesouros} tesouros:")
@@ -46,7 +49,9 @@ class JogoDoTesouroInca:
     """
     def __init__(self):
         self.templo = "Você está diante de um templo Inca."
-        self.camara = CamaraSecreta()
+        tesouro = CamaraSecreta(None)
+        self.camara = CamaraPerigosa(tesouro)
+        tesouro.outra = self.camara
         
     def vai(self):
         continua = " Você vai entrar? (s/N)"

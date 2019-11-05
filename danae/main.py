91bf326ce71f1fa95806e946123400a22348efec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 09:33:11 -0300

    Saved danae/main.py @2019-11-05 12:33

--- danae/main.py
+++ danae/main.py
@@ -3,22 +3,42 @@
 Uma aventura de exploração
 """
 __author__ = "Carlo Oliveira <carlo at ufrj br>"
-__version__ = "19.11.05c"
+__version__ = "19.11.05d"
+
+class CamaraPerigosa:
+    """ Uma camara contendo um conteúdo misterioso. 
+    O jogador entra nela quando se invoca o método vai
+    """
+    def __init__(self):
+        self.camara = "Você entrou numa câmara com perigos."
+        self.perigos = 0
+        
+    def vai(self):
+        continua = " Segue para outra câmara? (s/N)"
+        if input(self.camara+continua) == "s":
+            self.tesouros = self.tesouros + 1
+            return self.vai()
+        else:
+            input("Você volta para a entrada do templo")
+            return self.tesouros
 
 class CamaraSecreta:
     """ Uma camara contendo um conteúdo misterioso. 
     O jogador entra nela quando se invoca o método vai
     """
     def __init__(self):
-        self.camara = "Você entrou numa câmara vazia."
+        self.camara = "Você entrou numa câmara com tesouros."
+        self.tesouros = 0
         
     def vai(self):
         continua = " Segue para outra câmara? (s/N)"
         if input(self.camara+continua) == "s":
-            self.vai()
+            self.tesouros = self.tesouros + 1
+            return self.vai()
         else:
             input("Você volta para a entrada do templo")
-            return
+            input(f"Você sai do templo com {self.tesouros} tesouros:")
+            return self.tesouros
 
 class JogoDoTesouroInca:
     """ O jogo do tesouro inca. 
@@ -32,8 +52,7 @@ class JogoDoTesouroInca:
         continua = " Você vai entrar? (s/N)"
         if input(self.templo+continua) == "s":
             input("Você se embrenha no templo, e explora")
-            self.camara.vai()
-            input("Você sai do templo de mãos vazias")
+            muitos = self.camara.vai()
         else:
             input("Você sabiamente desiste desta loucura")
 

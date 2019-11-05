72fdfa0e768c25ad28c8daea77e7de594d8f9efd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 10:11:08 -0300

    Saved stacy/main.py @2019-11-05 13:11

--- stacy/main.py
+++ stacy/main.py
@@ -2,38 +2,73 @@
 """ Tesouro Inca - versão texto uma aventura de exploração """
 
 __author__ = "Victória Regina Caruzo victorialourencocaruzo@gmail.com"
-__version__ = "19.11.05a"
+__version__ = "19.11.05f"
+from random import randint
+
+class CamaraPerigosa:
+    """ Uma camara contendo um perigos mortais. O jogador entra nela quando se invoca o método vai """
+    def __init__(self, outra):
+        self.camara = "Você entrou numa câmara com perigos."
+        self.perigos = 0
+        self.outra = outra
+        
+    def sai(self):
+        input(F"Você sai do templo mas encontrou {self.perigos} perigos")    
+        
+    def vai(self):
+        continua = " Segue para outra câmara? (s/N)"
+        if input(self.camara+continua) == "s":
+            self.perigos = self.perigos + 1
+            if randint(0,9) > 3:
+                return self.outra.vai()
+            else:
+                return self.vai()
+        else:
+            input("Você volta para a entrada do templo")
+            self.sai()
+            self.outra.sai()
+            return self.tesouros            
 
 class CamaraSecreta:
     """ Uma camara contendo um conteúdo misterioso. O jogador entra nela quando se invoca o método vai """
-    def __init__(self):
+    def __init__(self, outra):
         self.camara = "Você entrou numa câmara com tesouros."
         self.tesouros = 0
+        self.outra = outra
+        
+    def sai(self):
+        input(F"Você sai do templo com {self.tesouros} tesouros:")
         
     def vai(self):
         continua = "Segue para outra câmara? (s/N)"
         if input(self.camara+continua) == "s":
             self.tesouros = self.tesouros + 1
-            return self.vai()
+            if randint(0,9) > 6:
+                return self.outra.vai()
+            else:
+                return self.vai()
         else:
             input("Você volta para a entrada do templo")
+            self.sai()
+            self.outra.sai()
             return self.tesouros
 
 class JogoDoTesouroInca:
     """ O jogo do tesouro inca. O jogo começa quando se invoca o método vai """
     def __init__(self):
-        self.templo = "Você está diante de um templo Inca"
-        self.camara = CamaraSecreta()
+        self.templo = "Você está diante de um templo Inca."
+        self.camara = CamaraPerigosa(tesouro)
+        tesouro.outra = self.camara
         
     def vai(self):
         continua = " Você vai entrar? (s/N)"
         if input(self.templo+continua) == "s":
             input("Você se embrenha no templo, e explora")
             muitos = self.camara.vai()
-            input(F"Você sai do templo com {muitos} tesouros")
+            
         else:
             input("Você sabiamente desiste desta loucura")
-        
+        a
 if __name__ == "__main__":
     tesouro = JogoDoTesouroInca()
     tesouro.vai()

818b36c812dafac4c4de1971c501ad80c0dd8620
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 10:25:19 -0300

    Saved soraya/__log__.py @2019-11-05 13:25

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -48,3 +48,10 @@ SyntaxError: invalid syntax
      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 05 2019 10:25:19.868 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  v 19.11.05g - o jogador foge se encontra dois perigos qualquer
+     ^
+SyntaxError: invalid syntax
+'''},

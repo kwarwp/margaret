694d398d5e319c0aded0891daba9d8b7e73ddefb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 08:54:45 -0300

    Saved danae/main.py @2019-11-05 11:54

--- danae/main.py
+++ danae/main.py
@@ -3,7 +3,7 @@
 Uma aventura de exploração
 """
 __author__ = "Carlo Oliveira <carlo at ufrj br>"
-__version__ = "19.11.05a"
+__version__ = "19.11.05b"
 
 
 class JogoDoTesouroInca:
@@ -11,10 +11,14 @@ class JogoDoTesouroInca:
     O jogo começa quando se invoca o método vai
     """
     def __init__(self):
-        self.templo = "Você está diante de um templo Inca"
+        self.templo = "Você está diante de um templo Inca."
         
     def vai(self):
-        print(self.templo)
+        continua = " Você vai entrar? (s/N)"
+        if input(self.templo+continua) == "s":
+            input("Você se embrenha no templo, e desaparece")
+        else:
+            input("Você sabiamente desiste desta loucura")
 
 
 if __name__ == "__main__":

df3f3dc1843f62d15b98c1467c77578229113ac9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 10:36:06 -0300

    Saved stacy/__log__.py @2019-11-05 13:36

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -2023,3 +2023,27 @@ Exception: <TypeError: arguments[($len - 1)] is undefined>
 'error': '''
 Exception: <TypeError: arguments[($len - 1)] is undefined>
 '''},
+{'date': 'Tue Nov 05 2019 10:36:05.833 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 91
+    tesouro.vai()
+  module <module> line 84
+    muitos = self.camara.vai()
+  module <module> line 35
+    return self.outra.vai()
+  module <module> line 63
+    return self.outra.vai()
+  module <module> line 25
+    tipo_do_perigo = self.tipos[randint(0,5)]
+IndexError: list index out of range
+'''},

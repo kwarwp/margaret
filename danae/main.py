52b7801007a5b76cfc53aa18a305f638988ffa05
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 08:39:00 -0300

    Created danae/main.py @2019-11-05 11:39

---
+++ danae/main.py
@@ -0,0 +1,11 @@
+# margaret.danae.main.py
+""" Tesouro Inca - versão texto
+Uma aventura de exploração
+"""
+__author__ = "Carlo Oliveira <carlo at ufrj br>"
+__version__ = "19.11.05a"
+
+
+if __name__ == "__main__":
+    tesouro = Tesouro()
+    tesouro.vai()

864bc0ef4337d77aa54b4d227753a17877c7b96c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 09:53:47 -0300

    Saved danae/__log__.py @2019-11-05 12:53

--- danae/__log__.py
+++ danae/__log__.py
@@ -23,3 +23,39 @@
     return self.tesouros
 AttributeError: 'CamaraPerigosa' object has no attribute 'tesouros'
 '''},
+{'date': 'Tue Nov 05 2019 09:53:47.191 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    tesouro.vai()
+  module <module> line 69
+    muitos = self.camara.vai()
+  module <module> line 25
+    return self.outra.vai()
+  module <module> line 48
+    return self.outra.vai()
+  module <module> line 25
+    return self.outra.vai()
+  module <module> line 48
+    return self.outra.vai()
+  module <module> line 25
+    return self.outra.vai()
+  module <module> line 48
+    return self.outra.vai()
+  module <module> line 25
+    return self.outra.vai()
+  module <module> line 48
+    return self.outra.vai()
+  module <module> line 30
+    return self.tesouros
+AttributeError: 'CamaraPerigosa' object has no attribute 'tesouros'
+'''},

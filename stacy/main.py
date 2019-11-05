4bb54defe84d410df8e9a438166b1e7c72d41fcc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 09:04:57 -0300

    Saved stacy/main.py @2019-11-05 12:04

--- stacy/main.py
+++ stacy/main.py
@@ -4,15 +4,30 @@
 __author__ = "Victória Regina Caruzo victorialourencocaruzo@gmail.com"
 __version__ = "19.11.05a"
 
-class JogoDoTesouroInca:
-    """ o jogo do tesouro inca. o jogo começa quando se invoca o método vai """
+class CamaraSecreta:
+    """ Uma camara contendo um conteúdo misterioso. O jogador entra nela quando se invoca o método vai """
     def __init__(self):
-        self.templo = "Você está diante de um templo Inca"
+        self.camara = "Você entrou numa câmara vazia.""
         
     def vai(self):
-        continua = "você vai entrar? (S/N)"
+        continua = "Segue para outra câmara? (S/N)"
+        if input(self.camara+continua) == "S"
+            self.vai()
+        else:
+            input("Você volta para a entrada do templo")
+            return
+
+class JogoDoTesouroInca:
+    """ O jogo do tesouro inca. O jogo começa quando se invoca o método vai """
+    def __init__(self):
+        self.templo = "Você está diante de um templo Inca"
+        self.camara = CamaraSecreta()
+        
+    def vai(self):
+        continua = "Você vai entrar? (S/N)"
         if input(self.templo+continua) == "S":
-            input("você se embrenha no templo, e desaparece")
+            input("Você se embrenha no templo, e explora")
+            self.camara.vai()
         else:
             input("Você sabiamente desiste desta loucura")
         

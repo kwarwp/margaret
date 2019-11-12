caaf3d9b80978a276ce7ffa1dfef8593fd513fa2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 16:53:39 -0300

    Saved stacy/__log__.py @2019-11-12 19:53

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -2234,3 +2234,25 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 12 2019 16:53:54.461 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 93
+    tesouro.vai()
+  module <module> line 86
+    muitos = self.camara.vai()
+  module <module> line 36
+    return self.outra.vai()
+  module <module> line 63
+    self.tesouro[tipo_do_tesouro] = self.tesouro[tipo_do_tesouro] + 1
+AttributeError: 'CamaraSecreta' object has no attribute 'tesouro'
+'''},

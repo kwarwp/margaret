b9e592e92221a6062790bc66c926548fef9e1394
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:05:28 -0300

    Saved soraya/__log__.py @2019-11-26 13:05

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -183,3 +183,25 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 26 2019 10:05:27.723 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 123
+    tesouro.vai()
+  module <module> line 116
+    muitos = self.camara.vai()
+  module <module> line 43
+    return self.outra.vai()
+  module <module> line 82
+    tipo_do_tesouro = self.tipos[randint(0,16)]
+AttributeError: 'CamaraSecreta' object has no attribute 'tipos'
+'''},

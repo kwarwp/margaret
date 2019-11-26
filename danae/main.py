32b96656e5cbb687ebda3354aedbd9030ef443d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 11:30:56 -0300

    Saved danae/main.py @2019-11-26 14:30

--- danae/main.py
+++ danae/main.py
@@ -2,6 +2,7 @@
 """ Tesouro Inca - versão texto
 Uma aventura de exploração
 
+v 19.11.26b - cria uma classe baralho com perigos e tesouros
 v 19.11.26a - encontra mais de um tesouro e avisa quantos tem
 v 19.11.05h - o jogador foge se encontra dois perigos do mesmo tipo
 v 19.11.05g - o jogador foge se encontra dois perigos quaisquer
@@ -11,75 +12,85 @@ __author__ = "Carlo Oliveira <carlo at ufrj br>"
 __version__ = "19.11.05h"
 from random import randint, shuffle
 
-class CamaraPerigosa:
-    """ Uma camara contendo um perigos mortais. 
-    O jogador entra nela quando se invoca o método vai
-    """
-    def __init__(self, outra):
-        self.tipos = ["aranha", "fogo", "mumia", "cobra", "desabamento"]
-        self.camara = "Você entrou numa câmara com {}."
-        self.perigos = {tipo :0 for tipo in self.tipos}
-        self.perigo_mortal = None
-        self.outra = outra
-        
-    def sai(self):
-        per_m = self.perigo_mortal
-        quantos = self.perigos[per_m] if per_m != None else 0
-        per_m = per_m if per_m != None else "perigo"
-        input(f"Você sai do templo mas encontrou {quantos} {per_m}s")
 
-    def vai(self):
-        continua = " Segue para outra câmara? (s/N)"
-        tipo_do_perigo = self.tipos[randint(0,4)]
-        if input(self.camara.format(tipo_do_perigo)+continua) == "s":
-            self.perigos[tipo_do_perigo] = self.perigos[tipo_do_perigo] + 1
-            if self.perigos[tipo_do_perigo] > 1 :
-                self.perigo_mortal = tipo_do_perigo
-                input("Você foge assustado para a entrada do templo")
-                self.sai()
-                self.outra.perde()
-                return self.perigos
-            
-            if randint(0,9) > 3:
-                return self.outra.vai()
-            else:
-                return self.vai()
-        else:
-            input("Você volta para a entrada do templo")
-            self.sai()
-            self.outra.sai()
-            return self.perigos
-
-class CamaraSecreta:
-    """ Uma camara contendo um conteúdo misterioso. 
-    O jogador entra nela quando se invoca o método vai.
-    O jogador pode encontrar um total entre 1 e 17 tesouros
+class Baralho:
     """
-    def __init__(self, outra):
-        self.camara = "Você entrou numa câmara com {} tesouros."
+    """
+    def __init__(self):
+        self.baralho = []
         self.tesouros = 0
-        self.outra = outra
+        perigos = ["aranha", "fogo", "mumia",
+                   "cobra", "desabamento"]*5
+        tesouros = list(range(1,17))
+        self.perigos = {tipo :0 for tipo in perigos}
+
+        # cria os perigos
+        for perigo in perigos:
+            self.baralho.append(
+                 CamaraPerigosa(baralho=self,tipo=perigo))
+        # cria os perigos
+        for tesouro in tesouros:
+            self.baralho.append(
+                 CamaraSecreta(baralho=self,tipo=tesouro))
+                 
+        shuffle(self.baralho)
+    def quantos_perigos(self, tipo):
+        """ verifica quantos perigos deste tipo já saiu
+        """
+        self.perigos[tipo] += 1
+        return self.perigos[tipo]
         
     def perde(self):
         input(f"Você fugiu do templo e perdeu {self.tesouros} tesouros:")
         
     def sai(self):
         input(f"Você sai do templo com {self.tesouros} tesouros:")
+    def vai(self):
+        self.baralho.pop().vai()
+    def acumula_tesouros(self, quantos):
+        self.tesouros += quantos
+
+class CamaraPerigosa:
+    """ Uma camara contendo um perigos mortais. 
+    O jogador entra nela quando se invoca o método vai
+    """
+    def __init__(self, baralho, tipo):
+        self.baralho, self.tipo = baralho, tipo
+        self.camara = f"Você entrou numa câmara com {self.tipo}."
+        
+    def foge(self):
+        input(f"Você foge do templo porque encontrou dois {self.tipo}s")
+
+    def vai(self):
+        continua = " Segue para outra câmara? (s/N)"
+        if input(self.camara+continua) == "s":
+            if self.baralho.quantos_perigos(self.tipo) > 1 :
+                self.foge()
+                self.baralho.perde()
+            else:
+                return self.baralho.vai()
+        else:
+            input("Você volta para a entrada do templo")
+            self.baralho.sai()
+
+class CamaraSecreta:
+    """ Uma camara contendo um conteúdo misterioso. 
+    O jogador entra nela quando se invoca o método vai.
+    O jogador pode encontrar um total entre 1 e 17 tesouros
+    """
+    def __init__(self, baralho, tipo):
+        self.baralho, self.tipo = baralho, tipo
+        self.camara = f"Você entrou numa câmara com {self.tipo} tesouros."
         
     def vai(self):
         continua = " Segue para outra câmara? (s/N)"
-        tesouros_aqui = randint(1,17)
-        if input(self.camara.format(tesouros_aqui)+continua) == "s":
-            self.tesouros = self.tesouros + tesouros_aqui
-            if randint(0,9) > 6:
-                return self.outra.vai()
-            else:
-                return self.vai()
+        if input(self.camara+continua) == "s":
+            self.baralho.acumula_tesouros(self.tipo)
+            
+            return self.baralho.vai()
         else:
             input("Você volta para a entrada do templo")
-            self.sai()
-            self.outra.sai()
-            return self.tesouros
+            self.baralho.sai()
 
 class JogoDoTesouroInca:
     """ O jogo do tesouro inca. 
@@ -87,15 +98,13 @@ class JogoDoTesouroInca:
     """
     def __init__(self):
         self.templo = "Você está diante de um templo Inca."
-        tesouro = CamaraSecreta(None)
-        self.camara = CamaraPerigosa(tesouro)
-        tesouro.outra = self.camara
+        self.baralho = Baralho()
         
     def vai(self):
         continua = " Você vai entrar? (s/N)"
         if input(self.templo+continua) == "s":
             input("Você se embrenha no templo, e explora")
-            muitos = self.camara.vai()
+            self.baralho.vai()
         else:
             input("Você sabiamente desiste desta loucura")
 

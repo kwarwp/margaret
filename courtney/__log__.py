df8dfe51bee3d34184cde378942cf2ce8fc2bf90
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:16:45 -0300

    Saved courtney/__log__.py @2019-11-26 13:16

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -330,3 +330,10 @@ IndentationError: unexpected indent
     inca.inicia()
 AttributeError: 'Inca' object has no attribute 'inicia'
 '''},
+{'date': 'Tue Nov 26 2019 10:16:43.705 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  Uma aventura de exploração
+       ^
+SyntaxError: invalid syntax
+'''},

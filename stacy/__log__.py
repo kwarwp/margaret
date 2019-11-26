3ade2c91c3910e134ba150920e014fd3c2d065a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:10:33 -0300

    Saved stacy/__log__.py @2019-11-26 13:10

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -2439,3 +2439,33 @@ NameError: name 'tipo_do_tesouros' is not defined
     din = self.tesouros[din] if din != None else 0
 TypeError: unhashable type: 'dict'
 '''},
+{'date': 'Tue Nov 26 2019 11:10:36.655 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 99
+    tesouro.vai()
+  module <module> line 92
+    muitos = self.camara.vai()
+  module <module> line 36
+    return self.outra.vai()
+  module <module> line 73
+    return self.vai()
+  module <module> line 71
+    return self.outra.vai()
+  module <module> line 38
+    return self.vai()
+  module <module> line 42
+    self.outra.sai()
+  module <module> line 61
+    din = self.tesouros[din] if din != None else 0
+TypeError: unhashable type: 'dict'
+'''},

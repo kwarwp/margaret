ca68a08cc11c16f2c6addb4a71e882d1fbed7def
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:56:11 -0300

    Saved soraya/__log__.py @2019-11-26 13:56

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -358,3 +358,25 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 26 2019 10:56:10.240 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 101
+    tesouro.vai()
+  module <module> line 94
+    muitos = self.camara.vai()
+  module <module> line 44
+    return self.vai()
+  module <module> line 42
+    return self.outra.vai()
+AttributeError: 'CamaraPerigosa' object has no attribute 'outra'
+'''},

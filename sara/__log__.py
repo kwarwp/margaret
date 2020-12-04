65e28026012508a95ae8ec5ee889abbc0c5c6ed7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-12-04 13:59:15 -0300

    Saved sara/__log__.py @2020-12-04 16:59

--- sara/__log__.py
+++ sara/__log__.py
@@ -182,3 +182,21 @@ NameError: name 'INVENTARIO' is not defined
     INVENTARIO.inicia()
 NameError: name 'INVENTARIO' is not defined
 '''},
+{'date': 'Fri Dec 04 2020 13:59:14.582 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 141
+    inca = Jogo()  # Inca()
+  module <module> line 129
+    INVENTARIO.inicia()
+NameError: name 'INVENTARIO' is not defined
+'''},

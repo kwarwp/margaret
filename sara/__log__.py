c1bcfce01f24d5bb3b5a21b7be396613df645da7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 10:01:26 -0300

    Saved sara/__log__.py @2022-07-14 13:01

--- sara/__log__.py
+++ sara/__log__.py
@@ -238,3 +238,21 @@ NameError: name 'INVENTARIO' is not defined
     self.cartas = [Perigo(
 NameError: name 'RDI' is not defined
 '''},
+{'date': 'Thu Jul 14 2022 10:01:25.715 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 141
+    inca = Jogo()  # Inca()
+  module <module> line 134
+    self.templo = Cena(DI["TEMPLO"])
+KeyError: TEMPLO
+'''},
